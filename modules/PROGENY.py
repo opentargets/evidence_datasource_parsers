d54cb3f6f119d471477e467544215854c9280863
ckong <ckong@ebi.ac.uk>
2018-03-13 11:12:29 +0000

    new parser

---
+++ modules/PROGENY.py
@@ -0,0 +1,283 @@
+from settings import Config
+from common.HGNCParser import GeneParser
+import sys
+import logging
+import datetime
+import opentargets.model.core as opentargets
+import opentargets.model.bioentity as bioentity
+import opentargets.model.evidence.core as evidence_core
+import opentargets.model.evidence.linkout as evidence_linkout
+import opentargets.model.evidence.association_score as association_score
+
+__copyright__ = "Copyright 2014-2018, Open Targets"
+__credits__   = ["ChuangKee Ong"]
+__license__   = "Apache 2.0"
+__version__   = "1.2.7"
+__maintainer__= "ChuangKee Ong"
+__email__     = ["ckong@ebi.ac.uk"]
+__status__    = "Production"
+
+''' TCGA cancer EFO mapping '''
+TUMOR_TYPE_EFO_MAP = {
+    'BLCA': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000292', 'label': 'bladder carcinoma'},
+    'BRCA': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000305', 'label': 'breast carcinoma'},
+    'HNSC': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000181', 'label': 'head and neck squamous cell carcinoma'},
+    'KIRC': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000349', 'label': 'clear cell renal carcinoma'},
+    'LIHC': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000182', 'label': 'hepatocellular carcinoma'},
+    'LUAD': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000571', 'label': 'lung adenocarcinoma'},
+    'LUSC': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000708', 'label': 'squamous cell lung carcinoma'},
+    'PRAD': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000673', 'label': 'prostate adenocarcinoma'},
+    'STAD': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000503', 'label': 'stomach adenocarcinoma'},
+    'THCA': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0002892', 'label': 'thyroid carcinoma'},
+    'UCEC': {'uri': 'http://www.ebi.ac.uk/efo/EFO_1000233', 'label': 'endometrial endometrioid adenocarcinoma'},
+    ##TODO uri & label needs update
+    'KICH': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000000', 'label': 'kidney chromophobe'},
+    'KIRP': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000000', 'label': 'kidney renal papillary cell carcinoma'},
+    'COREAD': {'uri': 'http://www.ebi.ac.uk/efo/EFO_0000000', 'label': 'colorectoral adenoma'}
+}
+
+''' TCGA cancer acronyms '''
+TUMOR_TYPE_MAP = {
+    'BLCA': 'bladder carcinoma',
+    'BRCA': 'breast carcinoma',
+    'HNSC': 'head and neck squamous cell carcinoma',
+    'KIRC': 'clear cell renal carcinoma',
+    'LIHC': 'hepatocellular carcinoma',
+    'LUAD': 'lung adenocarcinoma',
+    'LUSC': 'squamous cell lung carcinoma',
+    'PRAD': 'prostate adenocarcinoma',
+    'STAD': 'stomach adenocarcinoma',
+    'THCA': 'thyroid carcinoma',
+    'UCEC': 'endometrial endometrioid adenocarcinoma',
+    'KICH': 'kidney chromophobe',
+    'KIRP': 'kidney renal papillary cell carcinoma',
+    'COREAD': ''
+}
+
+SYMBOL_MAPPING = {
+    # TODO These symbols does not has Ensembl ID mappings, need alternative mapping
+    '''
+    i.e 'C15orf55': 'NUTM1',
+
+    ADRBK1
+    ADRBK2
+    BRE
+    C10orf2
+    CASC5
+    CECR1
+    CSRP2BP
+    DEFB131
+    ERBB2IP
+    FAM175A
+    FAM175B
+    FIGF
+    FYB
+    GYLTL1B
+    IKBKAP
+    INADL
+    KIAA0101
+    KIRREL
+    MINA
+    MRE11A
+    NGFRAP1
+    PARK2
+    PTRF
+    RQCD1
+    SEPP1
+    SHFM1
+    TCEB1
+    TCEB2
+    TCEB3
+    TCEB3B
+    TCEB3C
+    TCEB3CL
+    UFD1L
+    VPRBP
+    WBSCR17
+    WHSC1
+    WHSC1L1
+    '''
+}
+
+class PROGENY():
+    def __init__(self, es=None, r_server=None):
+        self.evidence_strings = list()
+        self.symbols = {}
+        self.logger = logging.getLogger(__name__)
+
+    def process_progeny(self):
+        gene_parser = GeneParser()
+        gene_parser._get_hgnc_data_from_json()
+
+        self.symbols = gene_parser.genes
+
+        self.build_evidence()
+        self.write_evidence()
+
+    def build_evidence(self, filename=Config.PROGENY_FILENAME):
+
+        now = datetime.datetime.now()
+
+        '''
+            build evidence.provenance_type object
+        '''
+        provenance_type = evidence_core.BaseProvenance_Type(
+            database=evidence_core.BaseDatabase(
+                id="PROGENY",
+                version='2018.04',
+                dbxref=evidence_core.BaseDbxref(url="https://saezlab.github.io/progeny/", id="PROGENY Pathway association analysis of TCGA tumor types", version="2018.04")),
+            literature=evidence_core.BaseLiterature(
+                references=[evidence_core.Single_Lit_Reference(lit_id="http://europepmc.org/abstract/MED/29295995")]
+            )
+        )
+        error = provenance_type.validate(logging)
+
+        if error > 0:
+            self.logger.error(provenance_type.to_JSON(indentation=4))
+            sys.exit(1)
+
+        with open(filename, 'r') as progeny_input:
+            n = 0
+
+            for line in progeny_input:
+                n +=1
+                if n>1:
+                    '''
+                        Pathway Cancer_type     logFC   AveExpr t       P.Value adj.P.Val       B       Sample_size
+                        VEGF    BRCA    82.5722740654623        -33.5309221366032       20.8841002773029        2.09462188741988e-55    2.93247064238783e-54    18.4517211174995        235
+                        Hypoxia KIRC    625.176272105012        317.864789440655        23.2893493089142        1.4197622817373e-50     1.98766719443222e-49    11.4589892248308        144
+                        p53     BRCA    -167.884630928143       11.9146505664384        -19.2135398711684       4.89209545526163e-50    3.42446681868314e-49    16.9318129144987        235
+                    '''
+                    '''
+                        pval    => pvalue of the SLAPenrichment of the pathway indicated in the cancer/tumor type
+                        fdr     => FDR percentage of the SLAPenrichment of the pathway in the cancer/tumor type
+                        logOdds => log10 odd ratio (number of patients with mutations in the pathway / number of expected patients with mutations in the pathway)
+                        exeeco  => exclusive coverage of the pathway = number patients with mutations in exactly 1 gene in the pathway / number of patients with mutations in at least one gene in the pathway.
+                    '''
+                    #(tumor_type, gene_symbol, mutFreq_dataset, pathway_id, mutFreq_pathway, pval, fdr, logodds, excco) = tuple(line.rstrip().split('\t'))
+                    (pathway_id, tumor_type, logfc, aveexpr, t, pval, fdr, b, sample) = tuple(line.rstrip().split('\t'))
+
+                    #pathway = pathway_id.split(":")
+                    #pathway_id = pathway[0].rstrip()
+                    #pathway_desc = pathway[1].rstrip()
+                    #TODO this is to be replaced with the actual Reactome pathway description
+                    #TODO this is a temporary placeholder until gene_symbol is identify for each evidence
+                    gene_symbol = 'BRAF'
+                    pathway_desc = 'Reactome pathway descriptions'
+                    '''
+                        build evidence.resource_score object
+                    '''
+                    resource_score = association_score.Pvalue(
+                        type="pvalue",
+                        method=association_score.Method(
+                            description="PROGENY Pathway association analysis of TCGA tumor types as described in XXX et al (20XX)",
+                            reference  ="http://europepmc.org/abstract/MED/29295995",
+                            url="https://saezlab.github.io/progeny/"
+                        ),
+                        value=float(pval)
+                    )
+
+                    evidenceString = opentargets.Literature_Curated()
+                    evidenceString.validated_against_schema_version = Config.VALIDATED_AGAINST_SCHEMA_VERSION
+                    evidenceString.access_level = "public"
+                    evidenceString.type = "affected_pathway"
+                    evidenceString.sourceID = "progeny"
+                    '''
+                        build unique_association_field object
+                    '''
+                    evidenceString.unique_association_fields = {}
+                    evidenceString.unique_association_fields['symbol'] = gene_symbol
+                    evidenceString.unique_association_fields['tumor_type_acronym'] = tumor_type
+                    evidenceString.unique_association_fields['tumor_type'] = TUMOR_TYPE_MAP[tumor_type]
+                    evidenceString.unique_association_fields['pathway_id'] = 'http://www.reactome.org/PathwayBrowser/#%s' % (pathway_id)
+                    evidenceString.unique_association_fields['efo_id'] = TUMOR_TYPE_EFO_MAP[tumor_type]['uri']
+
+                    target_type = 'http://identifiers.org/cttv.target/gene_evidence'
+                    ensembl_gene_id = None
+
+                    if gene_symbol in SYMBOL_MAPPING:
+                        gene_symbol = SYMBOL_MAPPING[gene_symbol]
+
+                    '''
+                        build target object,
+                    '''
+                    if gene_symbol in self.symbols:
+                        ensembl_gene_id = self.symbols[gene_symbol]
+
+                        evidenceString.target = bioentity.Target(
+                            id="http://identifiers.org/ensembl/{0}".format(ensembl_gene_id),
+                            target_name=gene_symbol,
+                            #TODO activity is a required field in target object, currently set as unknown
+                            activity="http://identifiers.org/cttv.activity/unknown",
+                            target_type=target_type
+                        )
+
+                        '''
+                            build disease object
+                        '''
+                        evidenceString.disease = bioentity.Disease(
+                            id=TUMOR_TYPE_EFO_MAP[tumor_type]['uri'],
+                            name=TUMOR_TYPE_EFO_MAP[tumor_type]['label']
+                        )
+
+                        '''
+                            build evidence object
+                        '''
+                        evidenceString.evidence = evidence_core.Literature_Curated()
+                        evidenceString.evidence.date_asserted = now.isoformat()
+                        evidenceString.evidence.is_associated = True
+                        #TODO check is this the correct evidence code "computational combinatorial evidence"
+                        evidenceString.evidence.evidence_codes = ["http://purl.obolibrary.org/obo/ECO_0000053"]
+                        evidenceString.evidence.provenance_type = provenance_type
+                        evidenceString.evidence.resource_score = resource_score
+
+                        '''
+                            build evidence.url object
+                        '''
+                        linkout = evidence_linkout.Linkout (
+                            url='http://www.reactome.org/PathwayBrowser/#%s'%(pathway_id),
+                            nice_name='%s'%(pathway_desc)
+                        )
+
+                        evidenceString.evidence.urls = [linkout]
+
+                        error = evidenceString.validate(logging)
+
+                        if error > 0:
+                            self.logger.error(evidenceString.to_JSON())
+                            sys.exit(1)
+
+                        self.evidence_strings.append(evidenceString)
+
+                    else:
+                        self.logger.error("%s is not found in Ensembl" % gene_symbol)
+
+            self.logger.info("%s evidence parsed"%(n-1))
+            self.logger.info("%s evidence created"%len(self.evidence_strings))
+
+        progeny_input.close()
+
+    def write_evidence(self, filename=Config.PROGENY_EVIDENCE_FILENAME):
+        self.logger.info("Writing PROGENY evidence strings")
+        with open(filename, 'w') as progeny_output:
+            n = 0
+            for evidence_string in self.evidence_strings:
+                n += 1
+                self.logger.info(evidence_string.disease.id[0])
+                # get max_phase_for_all_diseases
+                error = evidence_string.validate(logging)
+                if error == 0:
+                    progeny_output.write(evidence_string.to_JSON(indentation=None)+"\n")
+                else:
+                    self.logger.error("REPORTING ERROR %i" %n)
+                    self.logger.error(evidence_string.to_JSON(indentation=4))
+            progeny_output.close()
+
+'''
+def main():
+    import logging
+    logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    main()
+'''
