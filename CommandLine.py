d34988293eff2c8d0afbdff456d759bb96ff387c
ckong <ckong@ebi.ac.uk>
2018-03-16 11:06:20 +0000

    pointing to refactor GE module

--- CommandLine.py
+++ CommandLine.py
@@ -1,18 +1,21 @@
 import argparse
 import sys
+import logging
 
-from  modules.PheWAS import PhewasProcessor
+from modules.PheWAS import PhewasProcessor
 from modules.Gene2Phenotype import G2P
-from modules.GenomicsEnglandPanelApp import GE
+#from modules.GenomicsEnglandPanelApp import GE
 from modules.MouseModels import Phenodigm
 from modules.IntOGen import IntOGen
 from modules.SLAPEnrich import SLAPEnrich
 from modules.PROGENY import PROGENY
+from modules.GEPanelApp import GEPanelApp
 
 
 from settings import Config
 
 def main():
+    logger = logging.getLogger(__name__)
 
     parser = argparse.ArgumentParser(description='Open Targets evidence generator')
 
@@ -50,7 +53,8 @@ def main():
         phewas_processor.setup()
         phewas_processor.convert_phewas_catalog_evidence_json()
     elif args.genomicsengland:
-        GE().process_all()
+        GEPanelApp().process_ge()
+        #GE().process_all()
     elif args.intogen:
         IntOGen().process_intogen()
     elif args.gene2phenotype:
