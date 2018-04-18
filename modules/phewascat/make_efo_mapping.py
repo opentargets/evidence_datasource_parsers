89f65fc0d869fb66c0ddec5b73e8ff4bbb865737
Eliseo Papa <elipapa@users.noreply.github.com>
2018-04-18 10:58:24 +0100

    mappings uses verbose find_term

--- modules/phewascat/make_efo_mapping.py
+++ modules/phewascat/make_efo_mapping.py
@@ -1,4 +1,10 @@
+'''recreates mapping table
+
+TODO: should check if one already exists and prompt recreation
+'''
+
 import csv
+import os
 import logging
 from zipfile import ZipFile
 from io import BytesIO, TextIOWrapper
@@ -13,6 +19,8 @@ PHEWAS_CATALOG_URL = 'https://storage.googleapis.com/otar000-evidence_input/PheW
 PHEWAS_PHECODE_MAP_URL = 'https://phewascatalog.org/files/phecode_icd9_map_unrolled.csv.zip'
 
 def download_ic9_phecode_map(url=PHEWAS_PHECODE_MAP_URL):
+    '''phewascatalog maps ICD9 to a strange PheCode, but they provide mappings
+    '''
     with requests.get(url) as phecode_res:
         # let us state clearly that I hate zip files. use gzip people!
         phecode_zip = ZipFile(BytesIO(phecode_res.content))
@@ -30,41 +38,49 @@ def download_ic9_phecode_map(url=PHEWAS_PHECODE_MAP_URL):
 def main():
     phecode_to_ic9 = download_ic9_phecode_map()
     otmap = OnToma()
-    efofile = open('phewascat-diseaseterms.tsv', 'w', newline='')
-    efowriter = csv.writer(efofile, delimiter='\t')
-    '''find EFO term'''
+    moduledir = os.path.dirname(os.path.realpath(__file__))
+    efofile = open(os.path.join(moduledir, 'phewascat-diseaseterms.tsv'), 'w', newline='')
+    fieldnames = ['query', 'term', 'label', 'source', 'quality', 'action']
+    efowriter = csv.DictWriter(efofile, fieldnames, delimiter='\t')
+    efowriter.writeheader()
+
+    #find EFO term
     mapped = 0
-    with requests.get(PHEWAS_CATALOG_URL, stream=True) as r:
-        for i, row in enumerate(csv.DictReader(r.iter_lines(decode_unicode=True))):
+    processed = 0
+    with requests.get(PHEWAS_CATALOG_URL, stream=True) as req:
+        for processed, row in enumerate(csv.DictReader(req.iter_lines(decode_unicode=True))):
             logger.debug(row)
 
             #first try using the ICD9 code that we are given
             try:
                 efoid = otmap.find_term(phecode_to_ic9[row['phewas_code']],
-                                    code="ICD9CM")
+                                        code="ICD9CM", verbose=True)
             except KeyError as e:
                 logger.warning('Could not find %s in the phecode map', e)
                 efoid = None
 
             if efoid:
-                efowriter.writerow([efoid,'ICD9','match'])
-                mapped +=1
+                efoid['query'] = row['phewas_string']
+                efowriter.writerow(efoid)
+                mapped += 1
             else:
                 # if we cannot find an ICD9 match, try to match for string using
                 # OnToma
-                efomatch = otmap._find_term_from_string(row['phewas_string'])
-                if efomatch[0]:
-                    mapped +=1
+                efomatch = otmap.find_term(row['phewas_string'], verbose=True)
+                if efomatch:
+                    mapped += 1
+                    efomatch['query'] = row['phewas_string']
                     efowriter.writerow(efomatch)
                 else:
-                    efowriter.writerow([''])
+                    efowriter.writerow({'query': row['phewas_string']})
 
 
     efofile.close()
     click.echo("Completed. Parsed {} rows. "
-                        "Found {} EFOids. "
-                        "Skipped {} ".format(i,mapped,i-mapped)
-                        )
+               "Found {} EFOids. "
+               "Skipped {} ".format(processed+1,
+                                    mapped,
+                                    processed+1-mapped))
 
 
 
