fa278a14d80b5a5ea6275a5d7488b8c06ec3803c
Eliseo Papa <elipapa@users.noreply.github.com>
2018-04-19 23:18:12 +0100

    compute in memory and don't map every line, especially if they have already been computed

--- modules/phewascat/make_efo_mapping.py
+++ modules/phewascat/make_efo_mapping.py
@@ -10,22 +10,22 @@ import logging
 import argparse
 from zipfile import ZipFile
 from io import BytesIO, TextIOWrapper
+from pathlib import Path
 
 import click
 import requests
 from tqdm import tqdm
-from ontoma import OnToma
-
 from constants import *
 
-# #ontoma's logger is useful to find out mapping issues
-# from ontoma import logger as ontomalogger
-# ontomalogger.setLevel(logging.INFO)
+from ontoma import OnToma
+#ontoma's logger is useful to find out mapping issues, but it's a bit loud
+from ontoma import logger as ontomalogger
+ontomalogger.setLevel(logging.WARNING)
 
-logger = logging.getLogger(__name__)
+__log__ = logging.getLogger(__name__)
+__moduledir__ = Path(__file__).resolve().parent
+__modulename__ = Path(__file__).parent.name
 
-PHEWAS_CATALOG_URL = 'https://storage.googleapis.com/otar000-evidence_input/PheWAScatalog/phewas-catalog.csv'
-PHEWAS_PHECODE_MAP_URL = 'https://phewascatalog.org/files/phecode_icd9_map_unrolled.csv.zip'
 
 def download_ic9_phecode_map(url=PHEWAS_PHECODE_MAP_URL):
     '''phewascatalog maps ICD9 to a strange PheCode, but they provide mappings
@@ -50,70 +50,81 @@ def main():
                         help='force rewrite of existing mapping file')
     args = parser.parse_args()
 
-
     phecode_to_ic9 = download_ic9_phecode_map()
-    otmap = OnToma()
-    moduledir = os.path.dirname(os.path.realpath(__file__))
-    efo_fn = os.path.join(moduledir, 'phewascat-diseaseterms.tsv')
+    otmap = OnToma(exclude='zooma')
+    mappings = {}
+
+    efo_fn = str(__moduledir__ / 'phewascat-diseaseterms.tsv')
+    ## TODO this should be a constant I import from ontoma
     fieldnames = ['query', 'term', 'label', 'source', 'quality', 'action']
 
+    #TODO: check for mappings on github, rather then here.
+
+    ## check if we have mappings and if we should re-use them
     if (not args.overwrite) and os.path.exists(efo_fn):
-        logger.info('Reading existing mappings...')
+        __log__.info('Caching existing mappings...')
         with open(efo_fn, 'r', newline='') as efo_f:
             eforeader = csv.DictReader(efo_f, fieldnames, delimiter='\t')
-            #create a set of those terms that have been mapped succesfully before
-            done = {row['query'] for row in eforeader if row['term']}
+            #create a dict for those terms that have been already mapped
+            # succesfully (or have been manually curated)
+            mappings = {row['query']:row for row in eforeader if row['quality'] == 'match'}
 
+
+    ## use local phewascatalog.org file if it exists where you run the script.
+    if os.path.exists(os.path.join(str(__moduledir__), 'phewas-catalog.csv')):
+        __log__.warning('Reading phewascatalog.org local file')
+        catalog = tqdm(csv.DictReader(open(os.path.join(str(__moduledir__), 'phewas-catalog.csv'))),
+                       total=TOTAL_NUM_PHEWAS)
+    else:
+        session = requests.Session()
+        req = session.get(PHEWAS_CATALOG_URL, stream=True)
+        catalog = tqdm(csv.DictReader(req.iter_lines(decode_unicode=True)),
+                       total=TOTAL_NUM_PHEWAS)
+
+    ## do some mapping now
+    mappings = {}
+    for processed, row in enumerate(catalog):
+        __log__.debug(row)
+
+        # skip duplicates disease. we need to find the mapping only once for all
+        # evidences connected to that disease
+        if row['phewas_string'] in mappings: continue
+
+        #first try using the ICD9 code that we are given
+        try:
+            efoid = otmap.find_term(phecode_to_ic9[row['phewas_code']],
+                                    code="ICD9CM", verbose=True)
+        except KeyError as e:
+            __log__.warning('Could not find %s in the phewascatalog phecode map', e)
+            efoid = None
+
+        if efoid:
+            mappings[row['phewas_string']] = {**efoid, 'query': row['phewas_string']}
+        else:
+            # if we cannot find an ICD9 match, try to match for string using
+            # OnToma
+            efomatch = otmap.find_term(row['phewas_string'],
+                                        verbose=True, suggest=True)
+            if efomatch:
+                mappings[row['phewas_string']] = {**efomatch, 'query': row['phewas_string']}
+            else:
+                # no match gives you only the query and a blank line
+                mappings[row['phewas_string']] = {'query': row['phewas_string']}
+
+    ## write or append mappings to a file
     efo_file = open(efo_fn, 'w', newline='') if args.overwrite else open(efo_fn, 'a', newline='')
-
     efowriter = csv.DictWriter(efo_file, fieldnames, delimiter='\t')
     if args.overwrite:
-        logger.warning('Writing over existing files if any')
+        __log__.warning('Writing over existing files if any')
         efowriter.writeheader()
-
-    #find EFO term
-    mapped = 0
-    processed = 0
-    with requests.get(PHEWAS_CATALOG_URL, stream=True) as req:
-        catalog = tqdm(csv.DictReader(req.iter_lines(decode_unicode=True)),
-                        total=TOTAL_NUM_PHEWAS)
-        for processed, row in enumerate(catalog):
-            logger.debug(row)
-
-            if done and (row['phewas_string'] in done):
-                continue
-
-            #first try using the ICD9 code that we are given
-            try:
-                efoid = otmap.find_term(phecode_to_ic9[row['phewas_code']],
-                                        code="ICD9CM", verbose=True)
-            except KeyError as e:
-                logger.warning('Could not find %s in the phewascatalog phecode map', e)
-                efoid = None
-
-            if efoid:
-                efoid['query'] = row['phewas_string']
-                efowriter.writerow(efoid)
-                mapped += 1
-            else:
-                # if we cannot find an ICD9 match, try to match for string using
-                # OnToma
-                efomatch = otmap.find_term(row['phewas_string'], verbose=True)
-                if efomatch:
-                    mapped += 1
-                    efomatch['query'] = row['phewas_string']
-                    efowriter.writerow(efomatch)
-                else:
-                    # no match gives you only the query and a blank line
-                    efowriter.writerow({'query': row['phewas_string']})
-
-
+    efowriter.writerows(mappings.values())
     efo_file.close()
+
     click.echo("Completed. Parsed {} rows. "
                "Found {} EFOids. "
                "Skipped {} ".format(processed+1,
-                                    mapped,
-                                    processed+1-mapped))
+                                    len(mappings),
+                                    processed+1-len(mappings)))
 
 
 
