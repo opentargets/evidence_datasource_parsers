1992651d2fd834b863f945da4b5b8df52edf9087
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-10-01 11:21:16 +0100

    improve logging

--- common/RareDiseasesUtils.py
+++ common/RareDiseasesUtils.py
@@ -8,7 +8,7 @@ class RareDiseaseMapper(object):
     def __init__(self):
         print("init RareDiseaseMapper")
         super(RareDiseaseMapper, self).__init__()
-        self._logger = logging.getLogger(__name__)
+        self._logger = logging.getLogger(__name__+".RareDiseaseMapper")
         self.omim_to_efo_map = OrderedDict()
         self.zooma_to_efo_map = OrderedDict()
 
@@ -18,11 +18,16 @@ class RareDiseaseMapper(object):
         self._logger.info("OMIM to EFO parsing - response code %s" % response.status)
         line_count = 0
         for line in response.readlines():
+            line = line.decode('utf8').strip()
+            #if its an empty line after stripping, skip it
+            if len(line) == 0:
+                continue
+            self._logger.debug("Parsing line: %s",line)
             '''
             omim	efo_uri	efo_label	source	status
             '''
             line_count += 1
-            (omim, efo_uri, efo_label, source, status) = line.decode('utf8').strip().split("\t")
+            (omim, efo_uri, efo_label, source, status) = line.split("\t")
             if omim not in self.omim_to_efo_map:
                 self.omim_to_efo_map[omim] = []
             self.omim_to_efo_map[omim].append({'efo_uri': efo_uri, 'efo_label': efo_label})
@@ -34,6 +39,11 @@ class RareDiseaseMapper(object):
         self._logger.info("ZOOMA to EFO parsing - response code %s" % response.status)
         n = 0
         for line in response.readlines():
+            line = line.decode('utf8').strip()
+            #if its an empty line after stripping, skip it
+            if len(line) == 0:
+                continue
+            self._logger.debug("Parsing line: %s",line)
             '''
             STUDY	BIOENTITY	PROPERTY_TYPE	PROPERTY_VALUE	SEMANTIC_TAG	ANNOTATOR	ANNOTATION_DATE
             disease	Amyotrophic lateral sclerosis 1	http://www.ebi.ac.uk/efo/EFO_0000253
@@ -41,7 +51,7 @@ class RareDiseaseMapper(object):
             n +=1
             if n > 1:
                 #self._logger.info("[%s]"%line)
-                (study, bioentity, property_type, property_value, semantic_tag, annotator, annotation_date) = line.decode('utf8').strip().split("\t")
+                (study, bioentity, property_type, property_value, semantic_tag, annotator, annotation_date) = line.split("\t")
                 if property_value.lower() not in self.omim_to_efo_map:
                     self.zooma_to_efo_map[property_value.lower()] = []
                 self.zooma_to_efo_map[property_value.lower()].append({'efo_uri': semantic_tag, 'efo_label': semantic_tag})
