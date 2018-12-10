023cf34b423fe7d6721f5aa86750d2abb88e2051
Adam Faulconbridge <afaulconbridge@googlemail.com>
2018-12-10 13:42:02 +0000

    update to phewas to remove pjs as not compatible with schema ref

--- modules/phewascat/run.py
+++ modules/phewascat/run.py
@@ -13,14 +13,25 @@ from pathlib import Path
 
 
 import requests
-import obonet
 from tqdm import tqdm
-import python_jsonschema_objects as pjs
-from python_jsonschema_objects.validators import ValidationError
 
 from common.HGNCParser import GeneParser
 from common.Utils import mapping_on_github, ghmappings, DuplicateFilter
-from constants import *
+
+PHEWAS_CATALOG_URL = 'https://storage.googleapis.com/otar000-evidence_input/PheWAScatalog/phewas-catalog.csv'
+
+'''define once where evidence is coming from
+'''
+PROVENANCE = {'literature': {
+            "references":[{"lit_id":"http://europepmc.org/articles/PMC3969265"}]
+            },
+             "database":{
+                 "version":"2013-12-31T09:53:37+00:00",
+                "id":"PHEWAS Catalog"
+                }
+}
+
+TOTAL_NUM_PHEWAS = 4269549
 
 
 __log__ = logging.getLogger(__name__)
@@ -37,51 +48,24 @@ dup_filter = DuplicateFilter()
 __log__.addFilter(dup_filter)
 
 
-
-'''import the json schema and create python objects with a built-in validator
-'''
-__log__.info('downloading schema')
-schema = requests.get(PHEWAS_SCHEMA)
-__log__.debug("Downloaded the following schema: %s", schema)
-
-builder = pjs.ObjectBuilder(schema.json())
-ot_objects = builder.build_classes()
-__log__.debug('The schema contains %s', [str(x) for x in dir(ot_objects)])
-
-target = ot_objects['Target']
-disease = ot_objects['Disease']
-variant = ot_objects['Variant<anonymous>']
-gene2variant = ot_objects['Gene2variant<anonymous>']
-variant2disease = ot_objects['Variant2disease<anonymous>']
-__log__.info('python objects created from schema')
-
-
-def download_ic9_phecode_map(url=PHEWAS_PHECODE_MAP_URL):
-    with requests.get(url) as phecode_res:
-        # let us state clearly that I hate zip files. use gzip people!
-        phecode_zip = ZipFile(BytesIO(phecode_res.content))
-        # you can't just pass a ZipFile object as you would do a gZipfile
-        # object. You actually have to extract it and to do that you need to
-        # pass the name of the file it contains.
-        phecode_file = phecode_zip.open(phecode_zip.namelist()[0])
-
-    with TextIOWrapper(phecode_file) as phecode_map:
-        reader = csv.DictReader(phecode_map)
-        return {row['phecode']:row['icd9'] for row in reader}
-
-
 def make_disease(ontology_uri):
-    return disease(id=ontology_uri)
+    disease = {}
+    disease["id"] = ontology_uri
+    return disease
 
 def make_target(ensgid):
-    return target(target_type="http://identifiers.org/cttv.target/gene_evidence",
-                  id="http://identifiers.org/ensembl/{}".format(ensgid),
-                  activity="http://identifiers.org/cttv.activity/unknown")
+    target = {}
+    target['target_type'] = "http://identifiers.org/cttv.target/gene_evidence"
+    target['id'] = "http://identifiers.org/ensembl/{}".format(ensgid)
+    target['activity'] = "http://identifiers.org/cttv.activity/unknown"
+    return target
 
 
 def make_variant(rsid):
-    return variant(type="snp single",
-                   id="http://identifiers.org/dbsnp/{}".format(rsid))
+    variant = {}
+    variant['type'] = "snp single"
+    variant['id'] = "http://identifiers.org/dbsnp/{}".format(rsid)
+    return variant
 
 
 def make_gene2variant():
@@ -93,18 +77,21 @@ def make_gene2variant():
             }
 
 def make_variant2disease(pval):
-    return variant2disease(unique_experiment_reference='http://europepmc.org/articles/PMC3969265',
-                           provenance_type=PROVENANCE,
-                           is_associated=True,
-                           date_asserted="2013-12-31T09:53:37+00:00",
-                           evidence_codes=['http://identifiers.org/eco/PheWAS'],
-                           resource_score={'type': 'pvalue',
-                                           'method': {
-                                           "description":"pvalue for the phenotype to snp association."
-                                          },
-                            "value":float(pval)
-                            }
-                           )
+    variant2disease = {}
+    variant2disease['unique_experiment_reference']='http://europepmc.org/articles/PMC3969265'
+    variant2disease['provenance_type']=PROVENANCE
+    variant2disease['is_associated']=True
+    variant2disease['date_asserted']="2013-12-31T09:53:37+00:00"
+    variant2disease['evidence_codes']=['http://identifiers.org/eco/PheWAS']
+    variant2disease['resource_score']={
+        'type': 'pvalue',
+        'method': {
+            "description":"pvalue for the phenotype to snp association."
+            },
+        "value":float(pval)
+        }
+
+    return variant2disease
 
 
 
@@ -125,14 +112,14 @@ def main(outputdir):
                     mappings[row['query'].strip()] = row['term'].strip()
         __log__.info('Parsed %s mappings', len(mappings))
     else:
-        try:
-            moddir = os.path.dirname(os.path.realpath(__file__))
-            with open(os.path.join(moddir, 'phewascat-diseaseterms.tsv')) as mapf:
+        moddir = os.path.dirname(os.path.realpath(__file__))
+        inputfile = os.path.join(moddir, 'phewascat-diseaseterms.tsv')
+        if os.path.exists(inputfile) and os.path.isfile(inputfile):
+            with open(inputfile) as mapf:
                 reader = csv.DictReader(mapf,delimiter='\t')
-                mappings = {row['query']:row['term'] for row in reader
-                            if (row['quality'] and row['quality'] == 'match')}
-        except FileNotFoundError:
-            sys.exit('ABORTING. No existing mappings found. Please run make_efo_mapping.py first.')
+                mappings = {row['query']:row['term'] for row in reader if (row['quality'] and row['quality'] == 'match')}
+        else:
+            raise RuntimeError("Unable to read {}".format(inputfile))
 
     ## gene symbol <=> ENSGID mappings ##
     gene_parser = GeneParser()
@@ -140,12 +127,6 @@ def main(outputdir):
     gene_parser._get_hgnc_data_from_json()
     ensgid = gene_parser.genes
 
-    ## phewascatalog's Phecode <=> ICD9 mappings ##
-    phecode_to_ic9 = download_ic9_phecode_map()
-
-
-    ## prepare an object ##
-    PhewasEv = ot_objects['Genetics-basedEvidenceStrings']
 
     built = 0
     __log__.info('Begin processing phewascatalog evidences..')
@@ -156,55 +137,52 @@ def main(outputdir):
                            total=TOTAL_NUM_PHEWAS)
             for i, row in enumerate(catalog):
                 __log__.debug(row)
-                pev = PhewasEv(type = 'genetic_association',
-                               access_level = "public",
-                               sourceID = "phewas_catalog",
-                               validated_against_schema_version = '1.2.8'
-                              )
+
+                phewas_string = row['phewas_string']
+                gene = row['gene'].strip('*')
+                snp = row['snp']
+                row_p = row['p']
+                odds_ratio = row['odds_ratio']
+                cases = row['cases']
+
+                pev = {}
+                pev['type'] = 'genetic_association'
+                pev['access_level'] = 'public'
+                pev['sourceID'] = 'phewas_catalog'
+                pev['validated_against_schema_version'] = '1.2.8'
 
                 ## find EFO term ##
-
-                try:
-                    pev['disease'] = make_disease(mappings[row['phewas_string']])
-                except KeyError as e:
-                    __log__.error('No mapping for %s. Skipping evidence',e)
+                if phewas_string not in mappings:
+                    __log__.debug("Skipping unmapped disease %s",phewas_string)
                     continue
-                except ValidationError:
-                    __log__.error('Empty mapping for %s. '
-                                 'Skipping evidence',row['phewas_string'])
-                    continue
+                pev['disease'] = make_disease(mappings[phewas_string])
 
 
                 ## find ENSGID ##
-
-                try:
-                    pev['target'] = make_target(ensgid[row['gene'].strip('*')])
-                except ValidationError:
-                    __log__.error("Invalid gene: "
-                                  "http://identifiers.org/ensembl/%s",
-                                  ensgid[row['gene'].strip('*')])
+                if gene not in ensgid:
+                    __log__.debug("Skipping unmapped target %s",gene)
                     continue
-                except KeyError as e:
-                    __log__.error("Could not find gene: %s",row['gene'])
-                    #TODO: deal with the `intergenic` case by looking up the
-                    # postgap table or the snp2gene table in Open Targets
+                if not len(gene) or not len(ensgid[gene]):
+                    __log__.debug("Skipping zero length gene name")
                     continue
+                pev['target'] = make_target(ensgid[gene])
 
-                pev["variant"]= make_variant(row['snp'])
-                pev["evidence"] = { "variant2disease": make_variant2disease(row['p']),
+                pev["variant"]= make_variant(snp)
+                pev["evidence"] = { "variant2disease": make_variant2disease(row_p),
                                     "gene2variant": make_gene2variant() }
 
-                pev['unique_association_fields'] = {'odds_ratio':row['odds_ratio'],
-                                                    'cases' : row['cases'],
-                                                    'phenotype' : row['phewas_string']}
+                pev['unique_association_fields'] = {'odds_ratio':odds_ratio,
+                                                    'cases' : cases,
+                                                    'phenotype' : phewas_string}
 
                 built +=1
-                outfile.write("%s\n" % pev.serialize())
+                outfile.write("%s\n" % json.dumps(pev, 
+                    sort_keys=True, separators = (',', ':')))
 
         __log__.info("Completed. Parsed %s rows. "
-                    "Built %s evidences. "
-                    "Skipped %s ",i,built,i-built
-                    )
+            "Built %s evidences. "
+            "Skipped %s ",i,built,i-built
+            )
 
 
 if __name__ == '__main__':
