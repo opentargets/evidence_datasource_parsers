80fb8e451b547c84b8b7b67aee6ad0f3c025e7ab
MichaelaEBI <michaela@ebi.ac.uk>
2019-08-16 13:39:27 +0100

    formatting of code

--- modules/IntOGen.py
+++ modules/IntOGen.py
@@ -10,28 +10,28 @@ import opentargets.model.evidence.linkout as evidence_linkout
 import opentargets.model.evidence.association_score as association_score
 import opentargets.model.evidence.mutation as evidence_mutation
 
-__copyright__  = "Copyright 2014-2019, Open Targets"
-__credits__    = ["Gautier Koscielny", "David Tamborero", "ChuangKee Ong", "Michaela Spitzer"]
-__license__    = "Apache 2.0"
-__version__    = "1.2.6"
-__maintainer__ = "Open Targets Data Team"
-__email__      = ["data@opentargets.org"]
-__status__     = "Production"
+__copyright__ ="Copyright 2014-2019, Open Targets"
+__credits__   =["Gautier Koscielny", "David Tamborero", "ChuangKee Ong", "Michaela Spitzer"]
+__license__   ="Apache 2.0"
+__version__   ="1.2.6"
+__maintainer__="Open Targets Data Team"
+__email__     =["data@opentargets.org"]
+__status__    ="Production"
 
-INTOGEN_RELEASE_DATE = ''
-INTOGEN_SCORE_MAP = { 'A' : 0.75, 'B': 0.5, 'C': 0.25 }
-INTOGEN_SCORE_DOC = {
+INTOGEN_RELEASE_DATE=''
+INTOGEN_SCORE_MAP={ 'A' : 0.75, 'B': 0.5, 'C': 0.25 }
+INTOGEN_SCORE_DOC={
     'A' : 'the gene exhibits several signals of positive selection in the tumor type',
     'B' : 'the gene is already described as a cancer gene and exhibits a signal of positive selection in the tumor type',
     'C' : 'the gene exhibits a signal of positive selection and is functionally connected to the genes with evidence A or B in the tumor type'
 }
-INTOGEN_ROLE_MAP = {
+INTOGEN_ROLE_MAP={
     'Act' : 'http://identifiers.org/cttv.activity/gain_of_function',
     'LoF' : 'http://identifiers.org/cttv.activity/loss_of_function',
     'Ambiguous': 'http://identifiers.org/cttv.activity/unknown',
     'ambiguous': 'http://identifiers.org/cttv.activity/unknown'
 }
-INTOGEN_TUMOR_TYPE_EFO_MAP = {
+INTOGEN_TUMOR_TYPE_EFO_MAP={
     'ALL' : { 'uri' :'http://www.ebi.ac.uk/efo/EFO_0000220', 'label': 'acute lymphoblastic leukemia' },
     'AML' : { 'uri': 'http://www.ebi.ac.uk/efo/EFO_0000222', 'label' : 'acute myeloid leukemia' },
     'BLCA' : { 'uri' : 'http://www.ebi.ac.uk/efo/EFO_0000292', 'label' : 'bladder carcinoma' },
@@ -64,7 +64,7 @@ INTOGEN_TUMOR_TYPE_EFO_MAP = {
 }
 
 # Cancer acronyms
-INTOGEN_TUMOR_TYPE_MAP = {
+INTOGEN_TUMOR_TYPE_MAP={
     'ALL' : 'acute lymphocytic leukemia',
     'AML' : 'acute myeloid leukemia',
     'BLCA' : 'bladder carcinoma',
@@ -96,7 +96,7 @@ INTOGEN_TUMOR_TYPE_MAP = {
     'UCEC' : 'uterine corpus endometrioid carcinoma'
 }
 
-INTOGEN_SYMBOL_MAPPING = {
+INTOGEN_SYMBOL_MAPPING={
     'C15orf55' : 'NUTM1',
     'CSDA' : 'YBX3',
     'EIF2C3' : 'AGO3',
@@ -113,36 +113,34 @@ class IntOGen():
 
     def __init__(self):
 
-        self.evidence_strings = list()
-        self.genes = None
-        self.symbols = {}
-        self.logger = logging.getLogger(__name__)
+        self.evidence_strings=list()
+        self.genes=None
+        self.symbols={}
+        self.logger=logging.getLogger(__name__)
 
     def process_intogen(self, infile=Config.INTOGEN_FILENAME, outfile=Config.INTOGEN_EVIDENCE_FILENAME):
 
-        gene_parser = GeneParser()
+        gene_parser=GeneParser()
         gene_parser._get_hgnc_data_from_json()
-        self.genes = gene_parser.genes
+        self.genes=gene_parser.genes
         self.read_intogen(filename=infile)
         self.write_evidence_strings(filename=outfile)
 
     def read_intogen(self, filename=Config.INTOGEN_FILENAME):
 
-        records = []
-
         # the database was created in 2014
-        #now = datetime.datetime.now()
-        now = datetime.datetime(2014, 12, 1, 8, 30)
-        provenance_type = evidence_core.BaseProvenance_Type(
+        #now=datetime.datetime.now()
+        now=datetime.datetime(2014, 12, 1, 8, 30)
+        provenance_type=evidence_core.BaseProvenance_Type(
             database=evidence_core.BaseDatabase(
                 id="IntOGen Cancer Drivers Database",
                 version='2014.12',
                 dbxref=evidence_core.BaseDbxref(url="https://www.intogen.org/search", id="IntOGen Cancer Drivers Database", version="2014.12")),
-            literature = evidence_core.BaseLiterature(
-                references = [ evidence_core.Single_Lit_Reference(lit_id = "http://europepmc.org/abstract/MED/25759023") ]
+            literature=evidence_core.BaseLiterature(
+                references=[evidence_core.Single_Lit_Reference(lit_id="http://europepmc.org/abstract/MED/25759023")]
             )
         )
-        error = provenance_type.validate(self.logger)
+        error=provenance_type.validate(self.logger)
         print(error)
         if error > 0:
             self.logger.error(provenance_type.to_JSON(indentation=4))
@@ -150,95 +148,88 @@ class IntOGen():
             sys.exit(1)
 
         with open(filename, 'r') as intogen_file:
-            n = 0
+            n=0
             for line in intogen_file:
-                n +=1
-                if n>1:
+                n += 1
+                if n > 1:
+                    (Symbol, Ensg, Tumor_Type, Evidence, Role)=tuple(line.rstrip().split('\t'))
 
-                    (Symbol,Ensg,Tumor_Type,Evidence,Role) = tuple(line.rstrip().split('\t'))
-
-                    resource_score = association_score.Probability(
+                    resource_score=association_score.Probability(
                         type="probability",
                         method= association_score.Method(
-                            description ="IntOGen Driver identification methods as described in Rubio-Perez, C., Tamborero, D., Schroeder, MP., Antolin, AA., Deu-Pons,J., Perez-Llamas, C., Mestres, J., Gonzalez-Perez, A., Lopez-Bigas, N. In silico prescription of anticancer drugs to cohorts of 28 tumor types reveals novel targeting opportunities. Cancer Cell 27 (2015), pp. 382-396",
-                            reference = "http://europepmc.org/abstract/MED/25759023",
-                            url = "https://www.intogen.org/about"),
+                            description="IntOGen Driver identification methods as described in Rubio-Perez, C., Tamborero, D., Schroeder, MP., Antolin, AA., Deu-Pons,J., Perez-Llamas, C., Mestres, J., Gonzalez-Perez, A., Lopez-Bigas, N. In silico prescription of anticancer drugs to cohorts of 28 tumor types reveals novel targeting opportunities. Cancer Cell 27 (2015), pp. 382-396",
+                            reference="http://europepmc.org/abstract/MED/25759023",
+                            url="https://www.intogen.org/about"),
                         value=INTOGEN_SCORE_MAP[Evidence])
 
-                    evidenceString = opentargets.Literature_Curated()
-                    evidenceString.validated_against_schema_version = Config.VALIDATED_AGAINST_SCHEMA_VERSION
-                    evidenceString.access_level = "public"
-                    evidenceString.type = "somatic_mutation"
-                    evidenceString.sourceID = "intogen"
-                    evidenceString.unique_association_fields = {}
-                    evidenceString.unique_association_fields['projectName'] = 'IntOGen Cancer Drivers Database'
-                    evidenceString.unique_association_fields['symbol'] = Symbol
-                    evidenceString.unique_association_fields['tumor_type_acronym'] = Tumor_Type
-                    evidenceString.unique_association_fields['tumor_type'] = INTOGEN_TUMOR_TYPE_MAP[Tumor_Type]
-                    evidenceString.unique_association_fields['evidence_level'] = Evidence
-                    evidenceString.unique_association_fields['role'] = Role
-                    evidenceString.unique_association_fields['role_description'] = INTOGEN_ROLE_MAP[Role]
-                    evidenceString.unique_association_fields['method'] = 'OncodriveROLE'
-                    evidenceString.unique_association_fields['method_description'] = 'Classifying cancer driver genes into Loss of Function and Activating roles'
+                    evidenceString=opentargets.Literature_Curated()
+                    evidenceString.validated_against_schema_version=Config.VALIDATED_AGAINST_SCHEMA_VERSION
+                    evidenceString.access_level="public"
+                    evidenceString.type="somatic_mutation"
+                    evidenceString.sourceID="intogen"
+                    evidenceString.unique_association_fields={}
+                    evidenceString.unique_association_fields['symbol']=Symbol
+                    evidenceString.unique_association_fields['tumor_type_acronym']=Tumor_Type
+                    evidenceString.unique_association_fields['tumor_type']=INTOGEN_TUMOR_TYPE_MAP[Tumor_Type]
+                    evidenceString.unique_association_fields['evidence_level']=Evidence
+                    evidenceString.unique_association_fields['role']=Role
+                    evidenceString.unique_association_fields['role_description']=INTOGEN_ROLE_MAP[Role]
+                    evidenceString.unique_association_fields['method']='OncodriveROLE'
+                    evidenceString.unique_association_fields['method_description']='Classifying cancer driver genes into Loss of Function and Activating roles'
 
                     # target information (root.target.target_type is required)
                     # get the ensembl gene id from the symbol (mapping from 2014 won't work)
                     if Symbol in INTOGEN_SYMBOL_MAPPING:
-                        Symbol = INTOGEN_SYMBOL_MAPPING[Symbol]
+                        Symbol=INTOGEN_SYMBOL_MAPPING[Symbol]
 
-                    ensembl_gene_id = self.genes.get(Symbol)
+                    ensembl_gene_id=self.genes.get(Symbol)
                     if not ensembl_gene_id:
 
                         self.logger.error("%s is not found in Ensembl" % Symbol)
                         continue
 
-                    # id = [ "http://identifiers.org/ensembl/{0}".format(ensembl_gene_id) ], #["http://identifiers.org/ensembl/{0}".format(Ensg)],
-                    evidenceString.target = bioentity.Target(
-                        id= "http://identifiers.org/ensembl/{0}".format(ensembl_gene_id),
-                                            target_name = Symbol,
-                                            activity=INTOGEN_ROLE_MAP[Role],
-                                            target_type='http://identifiers.org/cttv.target/gene_evidence'
-                                            )
-
-#                    id = [INTOGEN_TUMOR_TYPE_EFO_MAP[Tumor_Type]['uri']],
-#                    name = [INTOGEN_TUMOR_TYPE_EFO_MAP[Tumor_Type]['label']]
+                    evidenceString.target=bioentity.Target(
+                        id="http://identifiers.org/ensembl/{0}".format(ensembl_gene_id),
+                        target_name=Symbol,
+                        activity=INTOGEN_ROLE_MAP[Role],
+                        target_type='http://identifiers.org/cttv.target/gene_evidence'
+                    )
                     # Disease information
-                    evidenceString.disease = bioentity.Disease(
-                                            id = INTOGEN_TUMOR_TYPE_EFO_MAP[Tumor_Type]['uri'],
-                                            name=INTOGEN_TUMOR_TYPE_EFO_MAP[Tumor_Type]['label']
-                                            )
-
+                    evidenceString.disease=bioentity.Disease(
+                        id=INTOGEN_TUMOR_TYPE_EFO_MAP[Tumor_Type]['uri'],
+                        name=INTOGEN_TUMOR_TYPE_EFO_MAP[Tumor_Type]['label']
+                    )
                     # Evidence
-                    evidenceString.evidence = evidence_core.Literature_Curated()
-                    evidenceString.evidence.date_asserted = now.isoformat()
-                    evidenceString.evidence.is_associated = True
-                    evidenceString.evidence.evidence_codes = [ "http://purl.obolibrary.org/obo/ECO_0000053" ]
-                    evidenceString.evidence.provenance_type = provenance_type
-                    evidenceString.evidence.resource_score = resource_score
+                    evidenceString.evidence=evidence_core.Literature_Curated()
+                    evidenceString.evidence.date_asserted=now.isoformat()
+                    evidenceString.evidence.is_associated=True
+                    evidenceString.evidence.evidence_codes=[ "http://purl.obolibrary.org/obo/ECO_0000053"]
+                    evidenceString.evidence.provenance_type=provenance_type
+                    evidenceString.evidence.resource_score=resource_score
 
-                    linkout = evidence_linkout.Linkout(
-                        url = 'https://www.intogen.org/search?gene=%s&cancer=%s'%(Symbol, Tumor_Type),
-                        nice_name = 'IntOGen -  %s gene cancer mutations in %s (%s)'%(Symbol, INTOGEN_TUMOR_TYPE_MAP[Tumor_Type], Tumor_Type)
+                    linkout=evidence_linkout.Linkout(
+                        url='https://www.intogen.org/search?gene=%s&cancer=%s'%(Symbol, Tumor_Type),
+                        nice_name='IntOGen -  %s gene cancer mutations in %s (%s)'%(Symbol, INTOGEN_TUMOR_TYPE_MAP[Tumor_Type], Tumor_Type)
                     )
 
-                    evidenceString.evidence.urls = [ linkout ]
+                    evidenceString.evidence.urls=[linkout]
 
                     # gain_of_function would become "Dominant", loss_of_function would be Recessive
-                    inheritance_pattern = 'unknown'
+                    inheritance_pattern='unknown'
                     if Role == 'Act':
-                        inheritance_pattern = 'dominant'
+                        inheritance_pattern='dominant'
                     elif Role == 'LoF':
-                        inheritance_pattern = 'recessive'
+                        inheritance_pattern='recessive'
 
                     # Gene_variant
-                    mutation = evidence_mutation.Mutation(
-                        functional_consequence = 'http://purl.obolibrary.org/obo/SO_0001564',
-                        preferred_name = 'gene_variant',
-                        inheritance_pattern = inheritance_pattern)
+                    mutation=evidence_mutation.Mutation(
+                        functional_consequence='http://purl.obolibrary.org/obo/SO_0001564',
+                        preferred_name='gene_variant',
+                        inheritance_pattern=inheritance_pattern)
 
-                    evidenceString.evidence.known_mutations = [ mutation ]
+                    evidenceString.evidence.known_mutations=[mutation]
 
-                    error = evidenceString.validate(logging)
+                    error=evidenceString.validate(logging)
                     if error > 0:
                         self.logger.error(evidenceString.to_JSON())
                         sys.exit(1)
@@ -254,11 +245,11 @@ class IntOGen():
     def write_evidence_strings(self, filename=Config.INTOGEN_EVIDENCE_FILENAME):
         self.logger.info("Writing IntOGen evidence strings")
         with open(filename, 'w') as tp_file:
-            n = 0
+            n=0
             for evidence_string in self.evidence_strings:
                 n+=1
                 self.logger.info(evidence_string.disease.id[0])
-                error = evidence_string.validate(logging)
+                error=evidence_string.validate(logging)
                 if error == 0:
                     tp_file.write(evidence_string.to_JSON(indentation=None)+"\n")
                 else:
@@ -268,7 +259,7 @@ class IntOGen():
         tp_file.close()
 
 def main():
-    into = IntOGen()
+    into=IntOGen()
     into.process_intogen()
 
 if __name__ == "__main__":
