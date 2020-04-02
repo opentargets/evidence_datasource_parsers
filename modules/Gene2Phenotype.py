2cdc0763fc402c37fd89712dec3bbc6cc67c0d6b
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2020-04-02 14:59:47 +0100

    [G2P] Remove imports of unused _model_ packages

--- modules/Gene2Phenotype.py
+++ modules/Gene2Phenotype.py
@@ -3,11 +3,6 @@ from common.HGNCParser import GeneParser
 from common.RareDiseasesUtils import RareDiseaseMapper
 
 import python_jsonschema_objects as pjo
-#import opentargets.model.core as opentargets
-#import opentargets.model.bioentity as bioentity
-#import opentargets.model.evidence.core as evidence_core
-#import opentargets.model.evidence.linkout as evidence_linkout
-#import opentargets.model.evidence.association_score as association_score
 
 import sys
 import logging
