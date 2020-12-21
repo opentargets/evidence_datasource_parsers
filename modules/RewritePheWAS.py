5ce2fcbb5776033483e7ca74f18da1026377cac6
Irene López <irene@Irenes-MacBook-Pro.local>
2020-12-21 14:58:19 +0000

    initiated parser

--- modules/RewritePheWAS.py
+++ modules/RewritePheWAS.py
@@ -46,9 +46,14 @@ class phewasEvidenceGenerator():
             on=["Phewas_string"],
             how="inner"
          )
-
+        # Build evidence strings per row
+        evidences = dataframe.rdd.map(
+            phewasEvidenceGenerator.parseEvidenceString)
+            .collect() # list of dictionaries
+        
         pass
-
+    
+    @staticmethod
     def parseEvidenceString(row):
         try:
             evidence = {
@@ -68,3 +73,23 @@ class phewasEvidenceGenerator():
             return evidence
         except Exception as e:
             raise        
+
+def main():
+    # Initiating parser
+    parser = argparse.ArgumentParser(description=
+    "This script generates evidences from the PheWAS Catalog data source.")
+
+    parser.add_argument("-i", "--inputFile", required=True, type=str, help="Input .tsv file with the table containing association details.")
+    parser.add_argument("-o", "--outputFile", required=True, type=str, help="Name of the json output file containing the evidence strings.")
+    parser.add_argument("-s", "--schemaVersion", required=True, type=str, help="JSON schema version to use, e.g. 1.6.9. It must be branch or a tag available in https://github.com/opentargets/json_schema.")
+    parser.add_argument("-m", "--mappingStep", required=True, type=bool, default=True, help="State whether to run the disease to EFO term mapping step or not.")
+
+    # Parsing parameters
+    args = parser.parse_args()
+
+    dataframe = args.inputFile
+    outputFile = args.outputFile
+    schemaVersion = args.schemaVersion
+    mappingStep = args.mappingStep
+
+    pass
