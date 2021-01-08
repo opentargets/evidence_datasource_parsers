980535e6feeb5de08cd3646c417d4c42d442b827
Irene López <irene@Irenes-MacBook-Pro.local>
2021-01-08 16:34:02 +0100

    variantId fixed according to new schema

--- modules/RewritePheWAS.py
+++ modules/RewritePheWAS.py
@@ -129,7 +129,7 @@ class phewasEvidenceGenerator():
                 studyCases : row["cases"],
                 targetFromSourceId : row["gene"].strip("*"),
                 variantFunctionalConsequenceId : "", # TODO : Merge this data coming from OTG
-                variantId : row["variantId"] if row["variantId"] else row["snp"]},
+                variantId : row["variantId"],
                 variantRsId : row["snp"]
             }
             return evidence
