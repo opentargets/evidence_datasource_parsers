bd9f5914a9e313b1f87241c9a2664fde79e56ba7
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2021-01-15 12:25:22 +0000

    [ClinGen] Remove gene symbol mapping to Ensembl

--- modules/ClinGen.py
+++ modules/ClinGen.py
@@ -1,5 +1,4 @@
 from settings import Config
-from common.HGNCParser import GeneParser
 import ontoma
 
 import python_jsonschema_objects as pjo
@@ -74,10 +73,6 @@ class ClinGen():
 
     def process_gene_validity_curations(self, in_filename, out_filename, unmapped_diseases_filename):
 
-        gene_parser = GeneParser()
-        gene_parser._get_hgnc_data_from_json()
-        self.genes = gene_parser.genes
-
         self.generate_evidence_strings(in_filename)
 
         # Save results to file
@@ -118,130 +113,125 @@ class ClinGen():
 
             gene_symbol.rstrip()
 
-            if gene_symbol in self.genes:
-                # Map gene symbol to ensembl
-                target = self.genes[gene_symbol]
-                ensembl_iri = "http://identifiers.org/ensembl/" + target
-
-                # Check that disease id exists in EFO or find equivalent term
-                # If id is not found in EFO OBO file the function returns None
-                if self.ontoma.get_efo_label(disease_id):
-                    disease_label = self.ontoma.get_efo_label(disease_id)
-                    # Create list of single disease to mimic what is returned by next step
-                    efo_mappings = [{'id': disease_id, 'name': disease_label}]
-                elif self.ontoma.get_efo_from_xref(disease_id):
-                    efo_mappings = self.ontoma.get_efo_from_xref(disease_id)
-                    self._logger.info("{} mapped to {} EFO ids based on xrefs.".format(disease_id, len(efo_mappings)))
-                else:
-                    # Search disease label using OnToma and accept perfect matches
-                    ontoma_mapping = self.ontoma.find_term(disease_name, verbose=True)
-                    if ontoma_mapping:
-                        if ontoma_mapping['action'] is None:
-                            efo_mappings = [{'id': ontoma_mapping['term'], 'name': ontoma_mapping['label']}]
-                        else:
-                            # OnToma fuzzy match ignored
-                            self._logger.info("Fuzzy match from OnToma ignored. Request EFO team to import {} - {}".format(disease_name, disease_id))
-                            # Record the unmapped disease
-                            self.unmapped_diseases.add((disease_id, disease_name))
-                            continue
+            # Check that disease id exists in EFO or find equivalent term
+            # If id is not found in EFO OBO file the function returns None
+            if self.ontoma.get_efo_label(disease_id):
+                disease_label = self.ontoma.get_efo_label(disease_id)
+                # Create list of single disease to mimic what is returned by next step
+                efo_mappings = [{'id': disease_id, 'name': disease_label}]
+            elif self.ontoma.get_efo_from_xref(disease_id):
+                efo_mappings = self.ontoma.get_efo_from_xref(disease_id)
+                self._logger.info("{} mapped to {} EFO ids based on xrefs.".format(disease_id, len(efo_mappings)))
+            else:
+                # Search disease label using OnToma and accept perfect matches
+                ontoma_mapping = self.ontoma.find_term(disease_name, verbose=True)
+                if ontoma_mapping:
+                    if ontoma_mapping['action'] is None:
+                        efo_mappings = [{'id': ontoma_mapping['term'], 'name': ontoma_mapping['label']}]
                     else:
-                        # MONDO id could not be found in EFO. Log it and continue
-                        self._logger.info("{} - {} could not be mapped to any EFO id. Skipping it, it should be checked with the EFO team".format(disease_name, disease_id))
+                        # OnToma fuzzy match ignored
+                        self._logger.info("Fuzzy match from OnToma ignored. Request EFO team to import {} - {}".format(disease_name, disease_id))
                         # Record the unmapped disease
                         self.unmapped_diseases.add((disease_id, disease_name))
                         continue
+                else:
+                    # MONDO id could not be found in EFO. Log it and continue
+                    self._logger.info("{} - {} could not be mapped to any EFO id. Skipping it, it should be checked with the EFO team".format(disease_name, disease_id))
+                    # Record the unmapped disease
+                    self.unmapped_diseases.add((disease_id, disease_name))
+                    continue
 
-                for efo_mapping in efo_mappings:
+            for efo_mapping in efo_mappings:
 
-                    # *** Disease info ***
-                    disease_info = {
-                        'id': ontoma.interface.make_uri(efo_mapping['id']),
-                        'name' : efo_mapping['name'],
-                        'source_name': disease_name
-                    }
+                # *** Disease info ***
+                disease_info = {
+                    'id': ontoma.interface.make_uri(efo_mapping['id']),
+                    'name' : efo_mapping['name'],
+                    'source_name': disease_name
+                }
 
-                    type = "genetic_literature"
+                type = "genetic_literature"
 
-                    provenance_type = {
-                        'database' : {
+                provenance_type = {
+                    'database' : {
+                        'id' : "ClinGen - Gene Validity Curations",
+                        'version' : file_created_date,
+                        'dbxref' : {
+                            'url': "https://search.clinicalgenome.org/kb/gene-validity",
                             'id' : "ClinGen - Gene Validity Curations",
-                            'version' : file_created_date,
-                            'dbxref' : {
-                                'url': "https://search.clinicalgenome.org/kb/gene-validity",
-                                'id' : "ClinGen - Gene Validity Curations",
-                                'version' : file_created_date
+                            'version' : file_created_date
 
-                            }
                         }
                     }
+                }
 
-                    # *** General properties ***
-                    access_level = "public"
-                    sourceID = "clingen"
-                    validated_against_schema_version = self.schema_version
+                # *** General properties ***
+                access_level = "public"
+                sourceID = "clingen"
+                validated_against_schema_version = self.schema_version
 
-                    # *** Target info ***
-                    target = {
-                        'id' : ensembl_iri,
-                        'activity' : "http://identifiers.org/cttv.activity/unknown",
-                        'target_type' : "http://identifiers.org/cttv.target/gene_evidence",
-                        'target_name' : gene_symbol
+                # *** Target info ***
+                target = {
+                    'id' : ensembl_iri,
+                    'activity' : "http://identifiers.org/cttv.activity/unknown",
+                    'target_type' : "http://identifiers.org/cttv.target/gene_evidence",
+                    'target_name' : gene_symbol
+                }
+                # http://www.ontobee.org/ontology/ECO?iri=http://purl.obolibrary.org/obo/ECO_0000204 -- An evidence type that is based on an assertion by the author of a paper, which is read by a curator.
+
+                # *** Evidence info ***
+                # Score based on disease confidence/ classification
+                if classification in ClinGen_classification2score:
+                    score = ClinGen_classification2score[classification]
+                else:
+                    self.logger.error('{} is not a recognised ClinGen classification, assigning an score of 0'.format(classification))
+                    score = 0
+                resource_score = {
+                    'type': "probability",
+                    'value': score
+                }
+
+                # Linkout
+                linkout = [
+                    {
+                        'url' : report_url,
+                        'nice_name' : 'Gene Validity Curations: {} - {} report'.format(gene_symbol, disease_name)
                     }
-                    # http://www.ontobee.org/ontology/ECO?iri=http://purl.obolibrary.org/obo/ECO_0000204 -- An evidence type that is based on an assertion by the author of a paper, which is read by a curator.
+                ]
 
-                    # *** Evidence info ***
-                    # Score based on disease confidence/ classification
-                    if classification in ClinGen_classification2score:
-                        score = ClinGen_classification2score[classification]
-                    else:
-                        self.logger.error('{} is not a recognised ClinGen classification, assigning an score of 0'.format(classification))
-                        score = 0
-                    resource_score = {
-                        'type': "probability",
-                        'value': score
-                    }
-
-                    # Linkout
-                    linkout = [
-                        {
-                            'url' : report_url,
-                            'nice_name' : 'Gene Validity Curations: {} - {} report'.format(gene_symbol, disease_name)
-                        }
-                    ]
-
-                    evidence = {
-                        'is_associated' : True,
-                        'confidence' : classification,
-                        'allelic_requirement' : mode_of_inheritancr,
-                        'evidence_codes' : ["http://purl.obolibrary.org/obo/ECO_0000204"],
-                        'provenance_type' : provenance_type,
-                        'date_asserted' : date,
-                        'resource_score' : resource_score,
-                        'urls' : linkout
-                    }
-                    # *** unique_association_fields ***
-                    unique_association_fields = {
-                        'target_id' : ensembl_iri,
-                        'disease_id' : disease_id,
-                        'report_url' : report_url
-                    }
+                evidence = {
+                    'is_associated' : True,
+                    'confidence' : classification,
+                    'allelic_requirement' : mode_of_inheritancr,
+                    'evidence_codes' : ["http://purl.obolibrary.org/obo/ECO_0000204"],
+                    'provenance_type' : provenance_type,
+                    'date_asserted' : date,
+                    'resource_score' : resource_score,
+                    'urls' : linkout
+                }
+                # *** unique_association_fields ***
+                unique_association_fields = {
+                    'target_id' : ensembl_iri,
+                    'disease_id' : disease_id,
+                    'report_url' : report_url
+                }
 
 
-                    try:
-                        evidence = self.evidence_builder.Opentargets(
-                            type = type,
-                            access_level = access_level,
-                            sourceID = sourceID,
-                            evidence = evidence,
-                            target = target,
-                            disease = disease_info,
-                            unique_association_fields = unique_association_fields,
-                            validated_against_schema_version = validated_against_schema_version
-                        )
-                        self.evidence_strings.append(evidence)
-                    except:
-                        self._logger.warning('Evidence generation failed for row: {}'.format(index))
-                        raise
+                try:
+                    evidence = self.evidence_builder.Opentargets(
+                        type = type,
+                        access_level = access_level,
+                        sourceID = sourceID,
+                        evidence = evidence,
+                        target = target,
+                        disease = disease_info,
+                        unique_association_fields = unique_association_fields,
+                        validated_against_schema_version = validated_against_schema_version
+                    )
+                    self.evidence_strings.append(evidence)
+                except:
+                    self._logger.warning('Evidence generation failed for row: {}'.format(index))
+                    raise
 
     def write_evidence_strings(self, filename):
         self._logger.info("Writing ClinGen evidence strings to %s", filename)
