24c8fc209928d024d48b75750954fbf9dcda0e16
dsuveges <suvi.dani@gmail.com>
2021-05-15 23:30:04 +0100

    Update ClinGen parser: output evidence with unmapped disease.

--- modules/ClinGen.py
+++ modules/ClinGen.py
@@ -1,38 +1,19 @@
-import ontoma
-
-import logging
-import pandas as pd
 import argparse
 import json
+import logging
 
+import pandas as pd
+import ontoma
 
 class ClinGen():
+
     def __init__(self):
 
         self.evidence_strings = list()
         self.unmapped_diseases = set()
 
-        # Configure logging
-        # Create logger
-        self._logger = logging.getLogger(__name__)
-        self._logger.setLevel(logging.INFO)
-
-        # Create console handler
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
-
-        # Create formatter
-        formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-
-        # Add formatter to ch
-        ch.setFormatter(formatter)
-
-        # Add ch to handler
-        self._logger.addHandler(ch)
-
         # Create OnToma object
         self.ontoma = ontoma.interface.OnToma()
-
 
     def process_gene_validity_curations(self, in_filename, out_filename, unmapped_diseases_filename):
 
@@ -48,9 +29,10 @@ class ClinGen():
     def generate_evidence_strings(self, filename):
 
         # When reading csv file skip header lines that don't contain column names
-        gene_validity_curation_df = pd.read_csv(filename, skiprows= [0,1,2,3,5], quotechar='"')
+        gene_validity_curation_df = pd.read_csv(filename, skiprows=[0, 1, 2, 3, 5], quotechar='"')
+
         for index, row in gene_validity_curation_df.iterrows():
-            self._logger.info("{} - {}".format(row["GENE SYMBOL"], row["DISEASE LABEL"]))
+            logging.info("{} - {}".format(row["GENE SYMBOL"], row["DISEASE LABEL"]))
 
             gene_symbol = row["GENE SYMBOL"]
             disease_name = row["DISEASE LABEL"]
@@ -62,15 +44,20 @@ class ClinGen():
 
             gene_symbol.rstrip()
 
+            # Default value for efo mappings: 
+            efo_mappings = [ None ]
+
             # Check that disease id exists in EFO or find equivalent term
             # If id is not found in EFO OBO file the function returns None
             if self.ontoma.get_efo_label(disease_id):
                 disease_label = self.ontoma.get_efo_label(disease_id)
                 # Create list of single disease to mimic what is returned by next step
                 efo_mappings = [{'id': disease_id, 'name': disease_label}]
+            
             elif self.ontoma.get_efo_from_xref(disease_id):
                 efo_mappings = self.ontoma.get_efo_from_xref(disease_id)
-                self._logger.info("{} mapped to {} EFO ids based on xrefs.".format(disease_id, len(efo_mappings)))
+                logging.info("{} mapped to {} EFO ids based on xrefs.".format(disease_id, len(efo_mappings)))
+            
             else:
                 # Search disease label using OnToma and accept perfect matches
                 ontoma_mapping = self.ontoma.find_term(disease_name, verbose=True)
@@ -79,56 +66,59 @@ class ClinGen():
                         efo_mappings = [{'id': ontoma_mapping['term'], 'name': ontoma_mapping['label']}]
                     else:
                         # OnToma fuzzy match ignored
-                        self._logger.info("Fuzzy match from OnToma ignored. Request EFO team to import {} - {}".format(disease_name, disease_id))
+                        logging.warning("Fuzzy match from OnToma ignored. Request EFO team to import {} - {}".format(disease_name, disease_id))
                         # Record the unmapped disease
                         self.unmapped_diseases.add((disease_id, disease_name))
-                        continue
+                        
                 else:
                     # MONDO id could not be found in EFO. Log it and continue
-                    self._logger.info("{} - {} could not be mapped to any EFO id. Skipping it, it should be checked with the EFO team".format(disease_name, disease_id))
+                    logging.warning("{} - {} could not be mapped to any EFO id. Skipping it, it should be checked with the EFO team".format(disease_name, disease_id))
+                    
                     # Record the unmapped disease
                     self.unmapped_diseases.add((disease_id, disease_name))
-                    continue
 
             for efo_mapping in efo_mappings:
 
-                linkout = [
-                    {
-                        'url' : report_url
-                    }
-                ]
-
                 evidence = {
-                    'datasourceId' : 'clingen',
-                    'datatypeId' : 'genetic_literature',
-                    'targetFromSourceId' : gene_symbol,
+                    'datasourceId': 'clingen',
+                    'datatypeId': 'genetic_literature',
+                    'targetFromSourceId': gene_symbol,
                     'diseaseFromSource': disease_name,
                     'diseaseFromSourceId': disease_id,
-                    'diseaseFromSourceMappedId': ontoma.interface.make_uri(efo_mapping['id']).split("/")[-1],
                     'allelicRequirements': [mode_of_inheritance],
                     'confidence': classification,
                     'studyId': expert_panel_name,
-                    'urls': linkout
+                    'urls': {'url': report_url}
                 }
+
+                # diseaseFromSourceMappedId might be None - in that case the key is skipped:
+                if efo_mapping:
+                    evidence['diseaseFromSourceMappedId'] = ontoma.interface.make_uri(efo_mapping['id']).split("/")[-1]
 
                 self.evidence_strings.append(evidence)
 
     def write_evidence_strings(self, filename):
-        self._logger.info("Writing ClinGen evidence strings to %s", filename)
+        logging.info("Writing ClinGen evidence strings to %s", filename)
         with open(filename, 'w') as tp_file:
             for evidence_string in self.evidence_strings:
                 json.dump(evidence_string, tp_file)
                 tp_file.write("\n")
 
     def write_unmapped_diseases(self, filename):
-        self._logger.info("Writing ClinGen diseases not mapped to EFO to %s", filename)
+        logging.info("Writing ClinGen diseases not mapped to EFO to %s", filename)
         with open(filename, 'w') as unmapped_diseases_file:
             unmapped_diseases_file.write("disease_id\tdisease_name\n")
             for unmapped_disease in self.unmapped_diseases:
                 unmapped_diseases_file.write(unmapped_disease[0] + "\t" + unmapped_disease[1] + "\n")
 
 
-def main():
+def main(infile, outfile, unmapped_diseases_file):
+
+    clingen = ClinGen()
+    clingen.process_gene_validity_curations(infile, outfile, unmapped_diseases_file)
+
+
+if __name__ == "__main__":
 
     # Parse CLI arguments
     parser = argparse.ArgumentParser(description='Parse ClinGen gene-disease associations from Gene Validity Curations')
@@ -141,15 +131,25 @@ def main():
     parser.add_argument('-u', '--unmapped_diseases_file',
                         help='If specified, the diseases not mapped to EFO will be stored in this file',
                         type=str, default=False)
+    parser.add_argument('-l', '--log_file', type=str,
+                        help='Optional filename to redirect the logs into.')
 
     args = parser.parse_args()
+
     # Get parameters
     infile = args.input_file
     outfile = args.output_file
     unmapped_diseases_file = args.unmapped_diseases_file
+    log_file = args.log_file
 
-    clingen = ClinGen()
-    clingen.process_gene_validity_curations(infile, outfile, unmapped_diseases_file)
+    # Initialize the logger based on the provided log file. If no log file is specified, logs are written to STDERR.
+    logging_config = {
+        'level': logging.INFO,
+        'format': '%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
+        'datefmt': '%Y-%m-%d %H:%M:%S',
+    }
+    if log_file:
+        logging_config['filename'] = log_file
+    logging.basicConfig(**logging_config)
 
-if __name__ == "__main__":
-    main()
+    main(infile, outfile, unmapped_diseases_file)
