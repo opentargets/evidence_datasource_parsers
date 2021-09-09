a552ac9319fe8d695d388dd9055eed453d99808b
Irene López <irene.lopezs@protonmail.com>
2021-09-09 15:00:13 +0100

    Improved ontology class

---
+++ common/ontology.py
@@ -0,0 +1,58 @@
+import logging
+import random
+import time
+
+from ontoma.interface import OnToma
+from pandarallel import pandarallel
+
+ONTOMA_MAX_ATTEMPTS = 5
+pandarallel.initialize()
+
+
+def _ontoma_udf(row, ontoma_instance):
+    disease_name, disease_id = row['diseaseFromSource'], row['diseaseFromSourceId']
+    for attempt in range(1, ONTOMA_MAX_ATTEMPTS + 1):
+        # Try to map first by disease name (because that branch of OnToma is more stable), then by disease ID.
+        try:
+            mappings = ontoma_instance.find_term(query=disease_name, code=False)
+            if not mappings:
+                mappings = ontoma_instance.find_term(query=disease_id, code=True)
+            return [m.id_ot_schema for m in mappings]
+        except AttributeError:
+            return []
+        except:
+            # If this is not the last attempt, wait until the next one
+            if attempt != ONTOMA_MAX_ATTEMPTS:
+                time.sleep(10 + 30 * random.random())
+    logging.error(f'OnToma lookup failed for {disease_name!r} / {disease_id!r}')
+    return []
+
+
+def add_efo_mapping(evidence_strings, spark_instance, ontoma_cache_dir=None):
+    """Given evidence strings with diseaseFromSource and diseaseFromSourceId fields, try to populate EFO mapping
+    field diseaseFromSourceMappedId. In case there are multiple matches, the evidence strings will be exploded
+    accordingly."""
+    logging.info('Collect all distinct (disease name, disease ID) pairs.')
+    disease_info_to_map = (
+        evidence_strings
+        .select('diseaseFromSource', 'diseaseFromSourceId')
+        .distinct()
+        .toPandas()
+    )
+
+    logging.info('Initialise OnToma instance')
+    ontoma_instance = OnToma(cache_dir=ontoma_cache_dir)
+
+    logging.info('Map disease information to EFO.')
+    disease_info_to_map['diseaseFromSourceMappedId'] = disease_info_to_map.parallel_apply(
+        _ontoma_udf, args=(ontoma_instance,), axis=1
+    )
+    disease_info_to_map = disease_info_to_map.explode('diseaseFromSourceMappedId')
+
+    logging.info('Join the resulting information into the evidence strings.')
+    disease_info_df = spark_instance.createDataFrame(disease_info_to_map.astype(str))
+    return evidence_strings.join(
+        disease_info_df,
+        on=['diseaseFromSource', 'diseaseFromSourceId'],
+        how='left'
+    )
