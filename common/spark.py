bd13e6999f4ca46902887868fee1aca56a87475b
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2021-11-01 11:40:47 +0300

    Unify coalescing and writing the evidence strings

--- common/spark.py
+++ common/spark.py
@@ -1,4 +1,6 @@
+import os
 from psutil import virtual_memory
+import tempfile
 
 
 def detect_spark_memory_limit():
@@ -7,3 +9,15 @@ def detect_spark_memory_limit():
     of physical memory and allow Spark to use (almost) all of it."""
     mem_gib = virtual_memory().total >> 30
     return int(mem_gib * 0.9)
+
+
+def write_evidence_strings(evidence, output_file):
+    """Exports the table to a compressed JSON file containing the evidence strings."""
+    with tempfile.TemporaryDirectory() as tmp_dir_name:
+        (
+            evidence.coalesce(1).write.format('json').mode('overwrite')
+            .option('compression', 'org.apache.hadoop.io.compress.GzipCodec').save(tmp_dir_name)
+        )
+        json_chunks = [f for f in os.listdir(tmp_dir_name) if f.endswith('.json.gz')]
+        assert len(json_chunks) == 1, f'Expected one JSON file, but found {len(json_chunks)}.'
+        os.rename(os.path.join(tmp_dir_name, json_chunks[0]), output_file)
