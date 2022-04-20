81785b0a02e486f449742c24ff5fbb9a5da18282
Irene López <irene.lopezs@protonmail.com>
2022-04-20 16:52:55 +0100

    [EPMC] Change PySpark's Functions alias from `pf` to `F`

--- modules/EPMC.py
+++ modules/EPMC.py
@@ -5,7 +5,7 @@ import logging
 import sys
 
 from pyspark.sql.types import StringType
-import pyspark.sql.functions as pf
+import pyspark.sql.functions as F
 
 from common.evidence import initialize_sparksession, read_path, write_evidence_strings
 
@@ -67,21 +67,21 @@ def main(cooccurrenceFile, outputFile):
         read_path(cooccurrenceFile, spark)
         .repartition(200)
         # Filter out pairs found in unwanted sections
-        .filter(pf.col('section').isin(SECTIONS_OF_INTEREST))
+        .filter(F.col('section').isin(SECTIONS_OF_INTEREST))
         # Casting integer pmid column to string:
-        .withColumn("pmid", pf.trim(pf.col('pmid').cast(StringType())))
+        .withColumn("pmid", F.trim(F.col('pmid').cast(StringType())))
         # Publication identifier is a pmid if available, otherwise pmcid
         .withColumn(
-            'publicationIdentifier', pf.when(pf.col('pmid').isNull(), pf.col('pmcid')).otherwise(pf.col('pmid'))
+            'publicationIdentifier', F.when(F.col('pmid').isNull(), F.col('pmcid')).otherwise(F.col('pmid'))
         )
         # Filtering for disease/target cooccurrences:
         .filter(
-            (pf.col('type') == 'GP-DS')
-            & (pf.col('isMapped'))  # Filter gene/protein - disease cooccurrence
-            & (pf.col('publicationIdentifier').isNotNull())  # Filtering for mapped cooccurrences
-            & (pf.length(pf.col('text')) < 600)  # Making sure at least the pmid or the pmcid is given:
+            (F.col('type') == 'GP-DS')
+            & (F.col('isMapped'))  # Filter gene/protein - disease cooccurrence
+            & (F.col('publicationIdentifier').isNotNull())  # Filtering for mapped cooccurrences
+            & (F.length(F.col('text')) < 600)  # Making sure at least the pmid or the pmcid is given:
             & (  # Exclude sentences with more than 600 characters
-                pf.col('label1').isin(EXCLUDED_TARGET_TERMS) == False
+                F.col('label1').isin(EXCLUDED_TARGET_TERMS) == False
             )  # Excluding target labels from the exclusion list
         )
         # Renaming columns:
@@ -90,31 +90,31 @@ def main(cooccurrenceFile, outputFile):
         # Aggregating data by publication, target and disease:
         .groupBy(['publicationIdentifier', 'targetFromSourceId', 'diseaseFromSourceMappedId'])
         .agg(
-            pf.collect_set(pf.col('pmcid')).alias('pmcIds'),
-            pf.collect_set(pf.col('pmid')).alias('literature'),
-            pf.collect_set(
-                pf.struct(
-                    pf.col('text'),
-                    pf.col('start1').alias('tStart'),
-                    pf.col('end1').alias('tEnd'),
-                    pf.col('start2').alias('dStart'),
-                    pf.col('end2').alias('dEnd'),
-                    pf.col('section'),
+            F.collect_set(F.col('pmcid')).alias('pmcIds'),
+            F.collect_set(F.col('pmid')).alias('literature'),
+            F.collect_set(
+                F.struct(
+                    F.col('text'),
+                    F.col('start1').alias('tStart'),
+                    F.col('end1').alias('tEnd'),
+                    F.col('start2').alias('dStart'),
+                    F.col('end2').alias('dEnd'),
+                    F.col('section'),
                 )
             ).alias('textMiningSentences'),
-            pf.sum(pf.col('evidence_score')).alias('resourceScore'),
+            F.sum(F.col('evidence_score')).alias('resourceScore'),
         )
         # Nullify pmcIds if empty array
-        .withColumn('pmcIds', pf.when(pf.size('pmcIds') != 0, pf.col('pmcIds')))
+        .withColumn('pmcIds', F.when(F.size('pmcIds') != 0, F.col('pmcIds')))
         # Only evidence with score above 1 is considered:
-        .filter(pf.col('resourceScore') > 1)
+        .filter(F.col('resourceScore') > 1)
     )
 
     # Final formatting and saving data:
     evidence = (
         agg_cooccurrence_df
         # Adding literal columns:
-        .withColumn('datasourceId', pf.lit('europepmc')).withColumn('datatypeId', pf.lit('literature'))
+        .withColumn('datasourceId', F.lit('europepmc')).withColumn('datatypeId', F.lit('literature'))
         # Reorder columns:
         .select(
             [
@@ -134,14 +134,14 @@ def main(cooccurrenceFile, outputFile):
     logging.info('EPMC disease target evidence saved.')
     logging.info(f'Number of evidence: {agg_cooccurrence_df.count()}')
     # Report on the number of diseases, targets and associations if loglevel == "debug" to avoid cost on computation time:
-    logging.debug(f"Number of publications: {agg_cooccurrence_df.select(pf.col('publicationIdentifier')).count()}")
-    logging.debug(f"Number of targets: {evidence.select(pf.col('targetFromSourceId')).distinct().count()}")
-    logging.debug(f"Number of diseases: {evidence.select(pf.col('diseaseFromSourceMappedId')).distinct().count()}")
+    logging.debug(f"Number of publications: {agg_cooccurrence_df.select(F.col('publicationIdentifier')).count()}")
+    logging.debug(f"Number of targets: {evidence.select(F.col('targetFromSourceId')).distinct().count()}")
+    logging.debug(f"Number of diseases: {evidence.select(F.col('diseaseFromSourceMappedId')).distinct().count()}")
     logging.debug(
-        f"Number of associations: {evidence.select(pf.col('diseaseFromSourceMappedId'), pf.col('targetFromSourceId')).dropDuplicates().count()}"
+        f"Number of associations: {evidence.select(F.col('diseaseFromSourceMappedId'), F.col('targetFromSourceId')).dropDuplicates().count()}"
     )
     logging.debug(
-        f"Number of publications without pubmed ID: {agg_cooccurrence_df.filter(pf.col('pmid').isNull()).select('pmcid').distinct().count()}"
+        f"Number of publications without pubmed ID: {agg_cooccurrence_df.filter(F.col('pmid').isNull()).select('pmcid').distinct().count()}"
     )
 
 
