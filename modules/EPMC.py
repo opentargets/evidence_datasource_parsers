8936b27c14414e739b96a7098d7dcee08a166cac
Daniel Suveges <daniel.suveges@protonmail.com>
2022-06-10 17:13:31 +0100

    Enforcing PMC id pattern at the parser level.

--- modules/EPMC.py
+++ modules/EPMC.py
@@ -70,6 +70,8 @@ def main(cooccurrences, outputFile):
         .filter(F.col('section').isin(SECTIONS_OF_INTEREST))
         # Casting integer pmid column to string:
         .withColumn("pmid", F.trim(F.col('pmid').cast(StringType())))
+        # Dropping pmcid values that violate schema:
+        .withColumn('pmcid', F.when(F.col('pmcid').rlike(r'^PMC\d+$'), F.col('pmcid')))
         # Publication identifier is a pmid if available, otherwise pmcid
         .withColumn('publicationIdentifier', F.when(F.col('pmid').isNull(), F.col('pmcid')).otherwise(F.col('pmid')))
         # Filtering for disease/target cooccurrences:
@@ -100,7 +102,7 @@ def main(cooccurrences, outputFile):
             ).alias('textMiningSentences'),
             F.sum(F.col('evidence_score')).alias('resourceScore'),
         )
-        # Nullify pmcIds if empty array
+        # Nullify pmcIds if empty array:
         .withColumn('pmcIds', F.when(F.size('pmcIds') != 0, F.col('pmcIds')))
         # Only evidence with score above 1 is considered:
         .filter(F.col('resourceScore') > 1)
