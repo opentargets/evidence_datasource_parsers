a738a6a9bb4b392e1f1b047b51704ff7e93d162b
Irene López <irene.lopezs@protonmail.com>
2022-06-16 16:41:42 +0100

    [AZ] Minor fixes

--- modules/AzGeneBurden.py
+++ modules/AzGeneBurden.py
@@ -5,12 +5,13 @@ import argparse
 import logging
 import sys
 
+from pyspark import SparkFiles
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.functions import array, col, explode, expr, lit, log10, pow, round, when
+from pyspark.sql.functions import array, col, lit, log10, pow, round, when
 from pyspark.sql.types import DoubleType, IntegerType
 
-from common.evidence import initialize_sparksession, write_evidence_strings
+from common.evidence import initialize_sparksession, read_trait_mappings, write_evidence_strings
 
 METHOD_DESC = {
     'ptv': 'Burden test carried out with PTVs with a MAF smaller than 0.1%.',
@@ -35,11 +36,9 @@ def main(az_binary_data: str, az_quant_data: str, az_trait_mappings: str, spark_
     logging.info(f'File with the AZ PheWAS Portal quantitative traits associations: {az_quant_data}')
 
     # Load data
-    az_trait_mappings_df = (
-        spark_instance.read.csv(az_trait_mappings, sep='\t', header=True)
-        .withColumn('EFO', explode(expr("regexp_extract_all(EFO, '([A-Za-z]+_[0-9]+)')")))
-        .distinct()
-    )
+    az_trait_mappings_df = read_trait_mappings(az_trait_mappings, 'AstraZeneca PheWAS Portal', spark_instance)
+    logging.info(f'{az_trait_mappings_df.count()} AZs trait mappings have been loaded.')
+
     az_phewas_df = (
         spark_instance.read.parquet(az_binary_data)
         # Renaming of some columns to match schemas of both binary and quantitative evidence
@@ -57,7 +56,7 @@ def main(az_binary_data: str, az_quant_data: str, az_trait_mappings: str, spark_
         )
         # Add mapped trait from GWAS Catalog WIP sheet
         # This is a temporary solution, as the AZ studies are not yet integrated into the GWAS Catalog
-        .join(az_trait_mappings_df, on='Phenotype', how='left')
+        .join(az_trait_mappings_df.withColumnRenamed('PROPERTY_VALUE', 'Phenotype'), on='Phenotype', how='left')
         .withColumn('pValue', col('pValue').cast(DoubleType()))
         .filter(col('pValue') <= 1e-7)
         .distinct()
@@ -151,7 +150,7 @@ def parse_az_phewas_evidence(az_phewas_df: DataFrame) -> DataFrame:
         .withColumn('cohortId', lit('UK Biobank 450k'))
         .withColumnRenamed('Gene', 'targetFromSourceId')
         .withColumnRenamed('Phenotype', 'diseaseFromSource')
-        .withColumnRenamed('EFO', 'diseaseFromSourceMappedId')
+        .withColumnRenamed('SEMANTIC_TAG', 'diseaseFromSourceMappedId')
         .withColumn('resourceScore', col('pValue'))
         .withColumn('pValueExponent', log10(col('pValue')).cast(IntegerType()) - lit(1))
         .withColumn('pValueMantissa', round(col('pValue') / pow(lit(10), col('pValueExponent')), 3))
@@ -187,7 +186,10 @@ def parse_az_phewas_evidence(az_phewas_df: DataFrame) -> DataFrame:
         .withColumnRenamed('CollapsingModel', 'statisticalMethod')
         .withColumn('statisticalMethodOverview', col('statisticalMethod'))
         .replace(to_replace=METHOD_DESC, subset=['statisticalMethodOverview'])
-        .withColumn('allelicRequirements', when(col('statisticalMethod') == 'rec', array(lit('recessive'))).otherwise(array(lit('dominant'))))
+        .withColumn(
+            'allelicRequirements',
+            when(col('statisticalMethod') == 'rec', array(lit('recessive'))).otherwise(array(lit('dominant'))),
+        )
         .select(to_keep)
         .distinct()
     )
