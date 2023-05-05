b77bebc2cb4dd9a152241fee7ffb8469e94e82e3
Daniel Suveges <daniel.suveges@protonmail.com>
2023-05-05 22:39:38 +0100

    feat:adding gene essentiality parser

---
+++ modules/Essentiality.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python3
+"""Parser for Target Enabling Packages (TEP) downloaded from the Structural Genomics Consortium website."""
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+
+import argparse
+import logging
+import sys
+from functools import reduce
+from pyspark.sql import SparkSession, functions as f, types as t, DataFrame, Column
+
+from common.evidence import detect_spark_memory_limit, write_evidence_strings
+from pyspark.sql import types as t, functions as f
+from pyspark import SparkFiles
+
+if TYPE_CHECKING:
+    from pyspark.sql import DataFrame, SparkSession
+
+
+class DepMapEssentiality:
+
+    # List of files to be used:
+    MODELS = "Model.csv"
+    ESSENTIAL_GENE_LIST = "CRISPRInferredCommonEssentials.csv"
+    GENE_EFFECTS = "CRISPRGeneEffect.csv"
+    GENE_EXPRESSION = "OmicsExpressionProteinCodingGenesTPMLogp1.csv"
+    MUTATION_HOTSPOTS = "OmicsSomaticMutationsMatrixHotspot.csv"
+    MUTATION_DAMAGING = "OmicsSomaticMutationsMatrixDamaging.csv"
+
+    def __init__(
+        self: DepMapEssentiality,
+        spark: SparkSession,
+        input_folder: str,
+        tissue_mapping_url: str,
+    ) -> None:
+
+        self.spark = spark
+
+        # Reading model data:
+        models_df = self._prepare_models(f"{input_folder}/{self.MODELS}")
+
+        # Get a list of essential genes:
+        essential_genes_df = self._prepare_essential_genes(
+            f"{input_folder}/{self.ESSENTIAL_GENE_LIST}"
+        )
+
+        # Get a list of effect for each gene/cell line pair:
+        gene_effect_df = self._read_and_melt(
+            f"{input_folder}/{self.GENE_EFFECTS}", "geneEffect"
+        )
+
+        # Read and process expression data:
+        expression_df = self._read_and_melt(
+            f"{input_folder}/{self.GENE_EXPRESSION}", "expression"
+        )
+
+        # Read and process hotspot mutation:
+        hotspots = self._read_and_melt(
+            f"{input_folder}/{self.MUTATION_HOTSPOTS}", "hotspotMutation"
+        ).filter(f.col("hotspotMutation") > 0)
+
+        # Read and process damaging mutation:
+        damaging_mutation = self._read_and_melt(
+            f"{input_folder}/{self.MUTATION_DAMAGING}", "damagingMutation"
+        ).filter(f.col("damagingMutation") > 0)
+
+        # Get tissue mapping:
+        tissue_mapping = self._get_tissue_mapping(tissue_mapping_url)
+
+        # Join all the data together:
+        self.essentials = (
+            # The melted effect table:
+            gene_effect_df
+            # Narrowing down the gene effect table to genes that are in the essential gene list:
+            .join(essential_genes_df, on="targetSymbol", how="inner")
+            # Joining with model information:
+            .join(models_df, on="depmapId", how="left")
+            # Joining expression data:
+            .join(expression_df, on=["targetSymbol", "depmapId"], how="left")
+            # Joining damaging mutation data:
+            .join(damaging_mutation, on=["targetSymbol", "depmapId"], how="left")
+            # Joining hotspot mutation data:
+            .join(hotspots, on=["targetSymbol", "depmapId"], how="left")
+            # Joining tissue mapping:
+            .join(tissue_mapping, on="oncotreeLineage", how="left")
+            # Format data:
+            .select(
+                "targetSymbol",
+                "depmapId",
+                # Cell-line fields:
+                "cellLineName",
+                f.col("modelId").alias("diseaseCellLineId"),
+                # Disease:
+                "diseaseFromSource",
+                # Tissue fields:
+                "tissueId",
+                f.when(f.col("tissueName").isNull(), "other")
+                .otherwise(f.col("tissueName"))
+                .alias("tissueName"),
+                # Parsing mutation:
+                f.when(f.col("damagingMutation").isNotNull(), "damaging")
+                .when(f.col("hotspotMutation").isNotNull(), "hotspot")
+                .alias("mutation"),
+                # Measured values:
+                "geneEffect",
+                "expression",
+            ).persist()
+        )
+
+        # We have to assert the data is properly generated before returning:
+        assert isinstance(self.essentials, DataFrame)
+
+    def get_essential_genes_dataframe(self) -> DataFrame:
+        return self.essentials
+
+    def get_stats_on_essentials(self) -> None:
+        entry_count = self.essentials.count()
+        gene_count = self.essentials.select("targetSymbol").distinct().count()
+        disease_count = self.essentials.select("diseaseFromSource").distinct().count()
+
+        print(f"Number of entries: {entry_count}")
+        print(f"Number of essential genes: {gene_count}")
+        print(f"Number of unique diseases: {disease_count}")
+
+    def _get_tissue_mapping(self, tissue_mapping_url: str) -> DataFrame:
+        # TODO: pandas needs to be phased out:
+        return self.spark.createDataFrame(pd.read_csv(tissue_mapping_url))
+
+    def _read_and_melt(self, filename: str, value_name: str) -> DataFrame:
+        # Reading csv into dataframe:
+        df = self.spark.read.csv(filename, sep=",", header=True).withColumnRenamed(
+            "_c0", "depmapId"
+        )
+
+        # Extracting cell lines:
+        genes = df.columns[1:]
+
+        # Generate unpivot expression:
+        unpivot_expression = f"""stack({len(genes)}, {', '.join([f"'{c}', `{c}`" for c in genes])}) as (gene_label, {value_name})"""
+
+        # Transform dataset:
+        return df.select("depmapId", f.expr(unpivot_expression)).select(
+            "depmapId",
+            self._extract_gene_symbol(f.col("gene_label")),
+            f.col(value_name).cast(t.FloatType()),
+        )
+
+    @staticmethod
+    def _extract_gene_symbol(gene_col: Column) -> Column:
+        return f.split(gene_col, " ").getItem(0).alias("targetSymbol")
+
+    def _prepare_models(self, model_file: str) -> DataFrame:
+        return self.spark.read.csv(model_file, sep=",", header=True).select(
+            f.col("ModelID").alias("depmapId"),
+            f.col("CellLineName").alias("cellLineName"),
+            f.col("SangerModelID").alias("modelId"),
+            f.col("OncotreeLineage").alias("oncotreeLineage"),
+            f.col("OncotreePrimaryDisease").alias("diseaseFromSource"),
+        )
+
+    def _prepare_essential_genes(self, essential_gene_file: str) -> DataFrame:
+        return self.spark.read.csv(essential_gene_file, sep=",", header=True).select(
+            self._extract_gene_symbol(f.col("Essentials"))
+        )
+
+
+def get_depmap_essentials(
+    spark: SparkSession, input_folder: str, tissue_mapping_url: str
+) -> DataFrame:
+    """Wrapper function around the DepMap gene essentiality parser.
+
+    Args:
+        spark (SparkSession):
+        input_folder (str): input folder
+        tissue_mapping_url (str): path to tissue mapping
+
+    Returns:
+        DataFrame: Parsed essentiality data in spark dataframe.
+    """
+    depmap_essentials = DepMapEssentiality(spark, input_folder, tissue_mapping_url)
+    depmap_essentials.get_stats_on_essentials()
+    return depmap_essentials.get_essential_genes_dataframe()
+
+
+def main(
+    spark: SparkSession,
+    depmap_input_folder: str,
+    tissue_mapping_url: str,
+    output_file: str,
+) -> None:
+    """Processing gene essentiality evidene based on different sources.
+
+    Args:
+        spark (SparkSession):
+        depmap_input_folder (str): Path to the input files for DepMap
+        tissue_mapping_url (str): Path to the tissue mappings URL
+        output_file (str): _description_
+    """
+    gene_essentiality = [
+        # Generate gene essentiality tables from depmap data:
+        get_depmap_essentials(spark, depmap_input_folder, tissue_mapping_url).persist(),
+        # Potential further sources will come here:
+    ]
+
+    # merging all dataset into a single table:
+    gene_essentiality_df = reduce(
+        lambda df1, df2: df1.unionByName(df2, allowMissingColumns=True),
+        gene_essentiality,
+    )
+
+    # Write output:
+    write_evidence_strings(gene_essentiality_df, output_file)
+
+
+def parse_command_line_parameters():
+    parser = argparse.ArgumentParser(
+        description="Generating gene essentiality annotation."
+    )
+
+    parser.add_argument(
+        "--depmap_input_folder",
+        help="Path to folder in which input files are stored from DepMap.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--depmap_tissue_mapping",
+        help="Cell/tissue to UBERON mappings for DepMap.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--output",
+        help="Gene essentiality annotation in compressed json.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--log_file",
+        help="Destination of the logs generated by this script. Defaults to None",
+        type=str,
+        nargs="?",
+        default=None,
+    )
+
+    return parser
