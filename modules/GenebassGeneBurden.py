036d29dcd76a2cc238d08bb97eba5a8995cadd21
Irene López <irene.lopezs@protonmail.com>
2023-08-08 11:16:22 +0200

    style: black formatting

--- modules/GenebassGeneBurden.py
+++ modules/GenebassGeneBurden.py
@@ -10,13 +10,17 @@ from pyspark.sql.dataframe import DataFrame
 import pyspark.sql.functions as F
 import pyspark.sql.types as T
 
-from common.evidence import initialize_sparksession, import_trait_mappings, write_evidence_strings
+from common.evidence import (
+    initialize_sparksession,
+    import_trait_mappings,
+    write_evidence_strings,
+)
 
 METHOD_DESC = {
-    'pLoF': 'Burden test carried out with rare pLOF variants.',
-    'missense|LC': 'Burden test carried out with rare missense variants including low-confidence pLOF and in-frame indels.',
-    'synonymous': 'Burden test carried out with rare synonymous variants.',
-    'pLoF|missense|LC': 'Burden test carried out with pLOF or missense variants.',
+    "pLoF": "Burden test carried out with rare pLOF variants.",
+    "missense|LC": "Burden test carried out with rare missense variants including low-confidence pLOF and in-frame indels.",
+    "synonymous": "Burden test carried out with rare synonymous variants.",
+    "pLoF|missense|LC": "Burden test carried out with pLOF or missense variants.",
 }
 
 
@@ -24,24 +28,24 @@ def main(genebass_data: str) -> DataFrame:
     """
     This module extracts and processes target/disease evidence from the raw Genebass Portal.
     """
-    logging.info(f'File with the Genebass gene burden results: {genebass_data}')
+    logging.info(f"File with the Genebass gene burden results: {genebass_data}")
 
     # Load data
     genebass_df = (
         SparkSession.getActiveSession()
         .read.parquet(genebass_data)
-        .filter(F.col('Pvalue_Burden') <= 6.7e-7)
+        .filter(F.col("Pvalue_Burden") <= 6.7e-7)
         .select(
-            'gene_id',
-            'annotation',
-            'n_cases',
-            'n_controls',
-            'trait_type',
-            'phenocode',
-            'description',
-            'Pvalue_Burden',
-            'BETA_Burden',
-            'SE_Burden',
+            "gene_id",
+            "annotation",
+            "n_cases",
+            "n_controls",
+            "trait_type",
+            "phenocode",
+            "description",
+            "Pvalue_Burden",
+            "BETA_Burden",
+            "SE_Burden",
         )
         .distinct()
         .repartition(200)
@@ -51,15 +55,17 @@ def main(genebass_data: str) -> DataFrame:
     # Write output
     evd_df = parse_genebass_evidence(genebass_df)
 
-    if evd_df.filter(F.col('resourceScore') == 0).count() != 0:
-        logging.exception('There are evidence with a P value of 0.')
+    if evd_df.filter(F.col("resourceScore") == 0).count() != 0:
+        logging.exception("There are evidence with a P value of 0.")
         raise AssertionError(
             f"There are {evd_df.filter(F.col('resourceScore') == 0).count()} evidence with a P value of 0."
         )
     if not 8_000 < evd_df.count() < 10_000:
-        logging.exception(f'Genebass number of evidence are different from expected: {evd_df.count()}')
-        raise AssertionError('Genebass number of evidence are different from expected.')
-    logging.info(f'{evd_df.count()} evidence strings have been processed.')
+        logging.exception(
+            f"Genebass number of evidence are different from expected: {evd_df.count()}"
+        )
+        raise AssertionError("Genebass number of evidence are different from expected.")
+    logging.info(f"{evd_df.count()} evidence strings have been processed.")
 
     return evd_df
 
@@ -73,27 +79,27 @@ def parse_genebass_evidence(genebass_df: DataFrame) -> DataFrame:
         evd_df: DataFrame with Genebass's data following the t/d evidence schema.
     """
     to_keep = [
-        'datasourceId',
-        'datatypeId',
-        'targetFromSourceId',
-        'diseaseFromSource',
-        'diseaseFromSourceId',
-        'diseaseFromSourceMappedId',
-        'pValueMantissa',
-        'pValueExponent',
-        'beta',
-        'betaConfidenceIntervalLower',
-        'betaConfidenceIntervalUpper',
-        'resourceScore',
-        'ancestry',
-        'ancestryId',
-        'projectId',
-        'cohortId',
-        'studySampleSize',
-        'studyCases',
-        'statisticalMethod',
-        'statisticalMethodOverview',
-        'literature',
+        "datasourceId",
+        "datatypeId",
+        "targetFromSourceId",
+        "diseaseFromSource",
+        "diseaseFromSourceId",
+        "diseaseFromSourceMappedId",
+        "pValueMantissa",
+        "pValueExponent",
+        "beta",
+        "betaConfidenceIntervalLower",
+        "betaConfidenceIntervalUpper",
+        "resourceScore",
+        "ancestry",
+        "ancestryId",
+        "projectId",
+        "cohortId",
+        "studySampleSize",
+        "studyCases",
+        "statisticalMethod",
+        "statisticalMethodOverview",
+        "literature",
     ]
 
     # WARNING: There are some associations with a p-value of 0.0 in Genebass.
@@ -104,41 +110,54 @@ def parse_genebass_evidence(genebass_df: DataFrame) -> DataFrame:
         f"There are {genebass_df.filter(F.col('Pvalue_Burden') == 0.0).count()} evidence with a p-value of 0.0."
     )
     minimum_pvalue = (
-        genebass_df.filter(F.col('Pvalue_Burden') > 0.0)
-        .agg({'Pvalue_Burden': 'min'})
-        .collect()[0]['min(Pvalue_Burden)']
+        genebass_df.filter(F.col("Pvalue_Burden") > 0.0)
+        .agg({"Pvalue_Burden": "min"})
+        .collect()[0]["min(Pvalue_Burden)"]
     )
     genebass_df = genebass_df.withColumn(
-        'Pvalue_Burden', F.when(F.col('Pvalue_Burden') == 0.0, F.lit(minimum_pvalue)).otherwise(F.col('Pvalue_Burden'))
+        "Pvalue_Burden",
+        F.when(F.col("Pvalue_Burden") == 0.0, F.lit(minimum_pvalue)).otherwise(
+            F.col("Pvalue_Burden")
+        ),
     )
 
     return (
-        genebass_df.withColumn('datasourceId', F.lit('gene_burden'))
-        .withColumn('datatypeId', F.lit('genetic_association'))
-        .withColumn('projectId', F.lit('Genebass'))
-        .withColumn('cohortId', F.lit('UK Biobank 450k'))
-        .withColumn('ancestry', F.lit('EUR'))
-        .withColumn('ancestryId', F.lit('HANCESTRO_0009'))
-        .withColumnRenamed('gene_id', 'targetFromSourceId')
-        .withColumnRenamed('description', 'diseaseFromSource')
-        .withColumnRenamed('phenocode', 'diseaseFromSourceId')
+        genebass_df.withColumn("datasourceId", F.lit("gene_burden"))
+        .withColumn("datatypeId", F.lit("genetic_association"))
+        .withColumn("projectId", F.lit("Genebass"))
+        .withColumn("cohortId", F.lit("UK Biobank 450k"))
+        .withColumn("ancestry", F.lit("EUR"))
+        .withColumn("ancestryId", F.lit("HANCESTRO_0009"))
+        .withColumnRenamed("gene_id", "targetFromSourceId")
+        .withColumnRenamed("description", "diseaseFromSource")
+        .withColumnRenamed("phenocode", "diseaseFromSourceId")
         .join(
             import_trait_mappings(),
-            on='diseaseFromSource',
-            how='left',
+            on="diseaseFromSource",
+            how="left",
         )
-        .withColumnRenamed('Pvalue_Burden', 'resourceScore')
-        .withColumn('pValueExponent', F.log10(F.col('resourceScore')).cast(T.IntegerType()) - F.lit(1))
-        .withColumn('pValueMantissa', F.round(F.col('resourceScore') / F.pow(F.lit(10), F.col('pValueExponent')), 3))
+        .withColumnRenamed("Pvalue_Burden", "resourceScore")
+        .withColumn(
+            "pValueExponent",
+            F.log10(F.col("resourceScore")).cast(T.IntegerType()) - F.lit(1),
+        )
+        .withColumn(
+            "pValueMantissa",
+            F.round(
+                F.col("resourceScore") / F.pow(F.lit(10), F.col("pValueExponent")), 3
+            ),
+        )
         # Genebass reports effect sizes as beta, regardless of the type of measured trait, like other studies
-        .withColumnRenamed('BETA_Burden', 'beta')
-        .withColumn('betaConfidenceIntervalLower', (F.col('beta') - F.col('SE_Burden')))
-        .withColumn('betaConfidenceIntervalUpper', (F.col('beta') + F.col('SE_Burden')))
-        .withColumn('studySampleSize', (F.col('n_cases') + F.coalesce('n_controls', F.lit(0))))
-        .withColumnRenamed('n_cases', 'studyCases')
-        .withColumnRenamed('annotation', 'statisticalMethod')
-        .withColumn('statisticalMethodOverview', F.col('statisticalMethod'))
-        .replace(to_replace=METHOD_DESC, subset=['statisticalMethodOverview'])
+        .withColumnRenamed("BETA_Burden", "beta")
+        .withColumn("betaConfidenceIntervalLower", (F.col("beta") - F.col("SE_Burden")))
+        .withColumn("betaConfidenceIntervalUpper", (F.col("beta") + F.col("SE_Burden")))
+        .withColumn(
+            "studySampleSize", (F.col("n_cases") + F.coalesce("n_controls", F.lit(0)))
+        )
+        .withColumnRenamed("n_cases", "studyCases")
+        .withColumnRenamed("annotation", "statisticalMethod")
+        .withColumn("statisticalMethodOverview", F.col("statisticalMethod"))
+        .replace(to_replace=METHOD_DESC, subset=["statisticalMethodOverview"])
         .withColumn("literature", F.array(F.lit("36778668")))
         .select(to_keep)
         .distinct()
@@ -146,40 +165,40 @@ def parse_genebass_evidence(genebass_df: DataFrame) -> DataFrame:
 
 
 def get_parser():
-    'Get parser object for script GenebassGeneBurden.py.'
+    "Get parser object for script GenebassGeneBurden.py."
     parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument(
-        '--genebass_data',
-        help='Input parquet files with Genebass\'s burden associations.',
+        "--genebass_data",
+        help="Input parquet files with Genebass's burden associations.",
         type=str,
         required=True,
     )
     parser.add_argument(
-        '--output',
-        help='Output gzipped json file following the gene_burden evidence data model.',
+        "--output",
+        help="Output gzipped json file following the gene_burden evidence data model.",
         type=str,
         required=True,
     )
     parser.add_argument(
-        '--log_file',
-        help='Destination of the logs generated by this script. Defaults to None',
+        "--log_file",
+        help="Destination of the logs generated by this script. Defaults to None",
         type=str,
-        nargs='?',
+        nargs="?",
         default=None,
     )
 
     return parser
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = get_parser().parse_args()
 
     # Logger initializer. If no log_file is specified, logs are written to stderr
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
+        format="%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
     if args.log_file:
         logging.config.fileConfig(filename=args.log_file)
@@ -193,4 +212,4 @@ if __name__ == '__main__':
     )
 
     write_evidence_strings(evd_df, args.output)
-    logging.info(f'Evidence strings have been saved to {args.output}. Exiting.')
+    logging.info(f"Evidence strings have been saved to {args.output}. Exiting.")
