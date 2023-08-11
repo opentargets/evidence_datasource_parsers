2d8e3385e7efeebd929676418848a2a192b4ce79
Irene López <irene.lopezs@protonmail.com>
2023-08-11 13:56:07 +0100

    refactor(TargetSafety): update direction of effect for adverse_events (#2921)

--- modules/TargetSafety.py
+++ modules/TargetSafety.py
@@ -36,8 +36,8 @@ def main(
     # Logger initializer. If no log_file is specified, logs are written to stderr
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
+        format="%(asctime)s %(levelname)s %(module)s - %(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
     if log_file:
         logging.config.fileConfig(filename=log_file)
@@ -49,46 +49,53 @@ def main(
     spark = initialize_sparksession()
     spark.sparkContext.addFile(adverse_events)
     spark.sparkContext.addFile(safety_risk)
-    logging.info('Remote files successfully added to the Spark Context.')
+    logging.info("Remote files successfully added to the Spark Context.")
 
     # Load and process the input files into dataframes
-    ae_df = process_adverse_events(SparkFiles.get(adverse_events.split('/')[-1]))
-    sr_df = process_safety_risk(SparkFiles.get(safety_risk.split('/')[-1]))
+    ae_df = process_adverse_events(SparkFiles.get(adverse_events.split("/")[-1]))
+    sr_df = process_safety_risk(SparkFiles.get(safety_risk.split("/")[-1]))
     toxcast_df = process_toxcast(toxcast)
     aopwiki_df = process_aop(aopwiki)
-    logging.info('Data has been processed. Merging...')
+    logging.info("Data has been processed. Merging...")
 
     # Combine dfs and group evidence
     evidence_unique_cols = [
-        'id',
-        'targetFromSourceId',
-        'event',
-        'eventId',
-        'datasource',
-        'effects',
-        'isHumanApplicable',
-        'literature',
-        'url'
+        "id",
+        "targetFromSourceId",
+        "event",
+        "eventId",
+        "datasource",
+        "effects",
+        "isHumanApplicable",
+        "literature",
+        "url",
     ]
     safety_dfs = [ae_df, sr_df, toxcast_df, aopwiki_df]
     safety_df = (
-        reduce(lambda df1, df2: df1.unionByName(df2, allowMissingColumns=True), safety_dfs)
+        reduce(
+            lambda df1, df2: df1.unionByName(df2, allowMissingColumns=True), safety_dfs
+        )
         # Collect biosample and study metadata by grouping on the unique evidence fields
         .groupBy(evidence_unique_cols)
         .agg(
-            F.collect_set(F.col('biosample')).alias('biosamples'),
-            F.collect_set(F.col('study')).alias('studies'),
+            F.collect_set(F.col("biosample")).alias("biosamples"),
+            F.collect_set(F.col("study")).alias("studies"),
         )
-        .withColumn('biosamples', F.when(F.size('biosamples') != 0, F.col('biosamples')))
-        .withColumn('studies', F.when(F.size('studies') != 0, F.col('studies')))
+        .withColumn(
+            "biosamples", F.when(F.size("biosamples") != 0, F.col("biosamples"))
+        )
+        .withColumn("studies", F.when(F.size("studies") != 0, F.col("studies")))
     )
 
     # Write output
-    logging.info('Evidence strings have been processed. Saving...')
+    logging.info("Evidence strings have been processed. Saving...")
     write_evidence_strings(safety_df, output)
-    logging.info(f'{safety_df.count()} evidence of safety liabilities have been saved to {output}. Exiting.')
+    logging.info(
+        f"{safety_df.count()} evidence of safety liabilities have been saved to {output}. Exiting."
+    )
 
     return 0
+
 
 def process_aop(aopwiki: str) -> DataFrame:
     """Loads and processes the AOPWiki input JSON."""
@@ -96,22 +103,28 @@ def process_aop(aopwiki: str) -> DataFrame:
     return (
         spark.read.json(aopwiki)
         # if isHumanApplicable is False, set it to null as the lack of applicability has not been tested - it only shows lack of data
-        .withColumn('isHumanApplicable', F.when(F.col('isHumanApplicable') != F.lit(True), F.col("isHumanApplicable")))
+        .withColumn(
+            "isHumanApplicable",
+            F.when(
+                F.col("isHumanApplicable") != F.lit(True), F.col("isHumanApplicable")
+            ),
+        )
         # data bug: some events have the substring "NA" at the start - removal and trim the string
-        .withColumn('event', F.trim(F.regexp_replace(F.col('event'), '^NA', '')))
+        .withColumn("event", F.trim(F.regexp_replace(F.col("event"), "^NA", "")))
         # data bug: effects.direction need to be in lowercase, this field is an enum
         .withColumn(
-            'effects',
+            "effects",
             F.transform(
-                F.col('effects'),
+                F.col("effects"),
                 lambda x: F.struct(
-                    F.lower(x.direction).alias('direction'),
-                    x.dosing.alias('dosing'))
-            )
+                    F.lower(x.direction).alias("direction"), x.dosing.alias("dosing")
+                ),
+            ),
         )
         # I need to convert the biosamples array into a struct so that data is parsed the same way as the rest of the sources
-        .withColumn('biosample', F.explode_outer('biosamples'))
+        .withColumn("biosample", F.explode_outer("biosamples"))
     )
+
 
 def process_adverse_events(adverse_events: str) -> DataFrame:
     """
@@ -141,34 +154,47 @@ def process_adverse_events(adverse_events: str) -> DataFrame:
     """
 
     ae_df = (
-        spark.read.csv(adverse_events, sep='\t', header=True)
+        spark.read.csv(adverse_events, sep="\t", header=True)
         .select(
-            F.col('ensemblId').alias('id'),
-            F.col('symptom').alias('event'),
-            F.col('efoId').alias('eventId'),
-            F.col('ref').alias('datasource'),
-            F.col('pmid').alias('literature'),
-            'url',
+            F.col("ensemblId").alias("id"),
+            F.col("symptom").alias("event"),
+            F.col("efoId").alias("eventId"),
+            F.col("ref").alias("datasource"),
+            F.col("pmid").alias("literature"),
+            "url",
             F.struct(
-                F.col('biologicalSystem').alias('tissueLabel'),
-                F.col('uberonCode').alias('tissueId'),
-                F.lit(None).alias('cellLabel'),
-                F.lit(None).alias('cellFormat'),
-                F.lit(None).alias('cellId'),
-            ).alias('biosample'),
-            F.split(F.col('effect'), '_').alias('effects'),
+                F.col("biologicalSystem").alias("tissueLabel"),
+                F.col("uberonCode").alias("tissueId"),
+                F.lit(None).alias("cellLabel"),
+                F.lit(None).alias("cellFormat"),
+                F.lit(None).alias("cellId"),
+            ).alias("biosample"),
+            F.split(F.col("effect"), "_").alias("effects"),
         )
         .withColumn(
-            'effects',
+            "effects",
             F.struct(
-                F.element_at(F.col('effects'), 1).alias('direction'), F.element_at(F.col('effects'), 2).alias('dosing')
+                F.when(
+                    F.col("effects")[1].contains("activation"),
+                    F.lit("Activation/Increase/Upregulation"),
+                )
+                .when(
+                    F.col("effects")[1].contains("inhibition"),
+                    F.lit("Inhibition/Decrease/Downregulation"),
+                )
+                .alias("direction"),
+                F.element_at(F.col("effects"), 2).alias("dosing"),
             ),
         )
     )
 
     # Multiple dosing effects need to be grouped in the same record.
-    effects_df = ae_df.groupBy('id', 'event', 'datasource').agg(F.collect_set(F.col("effects")).alias("effects"))
-    ae_df = ae_df.drop("effects").join(effects_df, on=["id", "event", "datasource"], how="left")
+    effects_df = ae_df.groupBy("id", "event", "datasource").agg(
+        F.collect_set(F.col("effects")).alias("effects")
+    )
+    ae_df = ae_df.drop("effects").join(
+        effects_df, on=["id", "event", "datasource"], how="left"
+    )
 
     return ae_df
 
@@ -199,34 +225,36 @@ def process_safety_risk(safety_risk: str) -> DataFrame:
     """
 
     return (
-        spark.read.csv(safety_risk, sep='\t', header=True)
+        spark.read.csv(safety_risk, sep="\t", header=True)
         .select(
-            F.col('ensemblId').alias('id'),
-            'event',
-            'eventId',
-            F.col('pmid').alias('literature'),
-            F.col('ref').alias('datasource'),
+            F.col("ensemblId").alias("id"),
+            "event",
+            "eventId",
+            F.col("pmid").alias("literature"),
+            F.col("ref").alias("datasource"),
             F.struct(
-                F.col('biologicalSystem').alias('tissueLabel'),
-                F.col('uberonId').alias('tissueId'),
-                F.lit(None).alias('cellLabel'),
-                F.lit(None).alias('cellFormat'),
-                F.lit(None).alias('cellId'),
-            ).alias('biosample'),
+                F.col("biologicalSystem").alias("tissueLabel"),
+                F.col("uberonId").alias("tissueId"),
+                F.lit(None).alias("cellLabel"),
+                F.lit(None).alias("cellFormat"),
+                F.lit(None).alias("cellId"),
+            ).alias("biosample"),
             F.struct(
-                F.col('liability').alias('description'), F.lit(None).alias('name'), F.lit(None).alias('type')
-            ).alias('study'),
+                F.col("liability").alias("description"),
+                F.lit(None).alias("name"),
+                F.lit(None).alias("type"),
+            ).alias("study"),
         )
         .withColumn(
-            'event',
-            F.when(F.col('datasource').contains('Force'), 'heart disease').when(
-                F.col('datasource').contains('Lamore'), 'cardiac arrhythmia'
+            "event",
+            F.when(F.col("datasource").contains("Force"), "heart disease").when(
+                F.col("datasource").contains("Lamore"), "cardiac arrhythmia"
             ),
         )
         .withColumn(
-            'eventId',
-            F.when(F.col('datasource').contains('Force'), 'EFO_0003777').when(
-                F.col('datasource').contains('Lamore'), 'EFO_0004269'
+            "eventId",
+            F.when(F.col("datasource").contains("Force"), "EFO_0003777").when(
+                F.col("datasource").contains("Lamore"), "EFO_0004269"
             ),
         )
     )
@@ -248,7 +276,7 @@ def process_toxcast(toxcast: str) -> DataFrame:
         eventId                       | null
 
     Ex. output record:
-     targetFromSourceId | ESR1
+    targetFromSourceId | ESR1
     event              | cell proliferation
     eventId            | null
     biosample          | {null, null, T47D...
@@ -257,68 +285,81 @@ def process_toxcast(toxcast: str) -> DataFrame:
     study              | {ACEA_ER_80hr, AC...
     """
 
-    return spark.read.csv(toxcast, sep='\t', header=True).select(
-        F.trim(F.col('official_symbol')).alias('targetFromSourceId'),
-        F.col('biological_process_target').alias('event'),
-        'eventId',
+    return spark.read.csv(toxcast, sep="\t", header=True).select(
+        F.trim(F.col("official_symbol")).alias("targetFromSourceId"),
+        F.col("biological_process_target").alias("event"),
+        "eventId",
         F.struct(
-            F.col('tissue').alias('tissueLabel'),
-            F.lit(None).alias('tissueId'),
-            F.col('cell_short_name').alias('cellLabel'),
-            F.col('cell_format').alias('cellFormat'),
-            F.lit(None).alias('cellId'),
-        ).alias('biosample'),
-        F.lit('ToxCast').alias('datasource'),
-        F.lit('https://www.epa.gov/chemical-research/exploring-toxcast-data-downloadable-data').alias('url'),
+            F.col("tissue").alias("tissueLabel"),
+            F.lit(None).alias("tissueId"),
+            F.col("cell_short_name").alias("cellLabel"),
+            F.col("cell_format").alias("cellFormat"),
+            F.lit(None).alias("cellId"),
+        ).alias("biosample"),
+        F.lit("ToxCast").alias("datasource"),
+        F.lit(
+            "https://www.epa.gov/chemical-research/exploring-toxcast-data-downloadable-data"
+        ).alias("url"),
         F.struct(
-            F.col('assay_component_endpoint_name').alias('name'),
-            F.col('assay_component_desc').alias('description'),
-            F.col('assay_format_type').alias('type'),
-        ).alias('study'),
+            F.col("assay_component_endpoint_name").alias("name"),
+            F.col("assay_component_desc").alias("description"),
+            F.col("assay_format_type").alias("type"),
+        ).alias("study"),
     )
+
 
 def get_parser():
     """Get parser object for script TargetSafety.py."""
     parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument(
-        '--toxcast',
-        help='Input table containing biological processes associated with relevant targets that have been observed in toxicity assays.',
+        "--toxcast",
+        help="Input table containing biological processes associated with relevant targets that have been observed in toxicity assays.",
         type=str,
         required=True,
     )
     parser.add_argument(
-        '--adverse_events',
-        help='Input TSV containing adverse events associated with targets that have been collected from relevant publications. Fetched from https://raw.githubusercontent.com/opentargets/curation/master/target_safety/adverse_effects.tsv.',
+        "--adverse_events",
+        help="Input TSV containing adverse events associated with targets that have been collected from relevant publications. Fetched from https://raw.githubusercontent.com/opentargets/curation/master/target_safety/adverse_effects.tsv.",
         type=str,
         required=True,
     )
     parser.add_argument(
-        '--safety_risk', help='Input TSV containing cardiovascular safety liabilities associated with targets that have been collected from relevant publications. Fetched from https://raw.githubusercontent.com/opentargets/curation/master/target_safety/safety_risks.tsv.', type=str, required=True
-    )
-    parser.add_argument(
-        '--aopwiki', help='Input JSON containing targets implicated in adverse outcomes as reported by the AOPWiki. Parsed from their source XML data.', type=str, required=True
-    )
-    parser.add_argument(
-        '--output', help='Output gzipped json file following the target safety liabilities data model.', type=str, required=True
-    )
-    parser.add_argument(
-        '--log_file',
-        help='Destination of the logs generated by this script. Defaults to None',
+        "--safety_risk",
+        help="Input TSV containing cardiovascular safety liabilities associated with targets that have been collected from relevant publications. Fetched from https://raw.githubusercontent.com/opentargets/curation/master/target_safety/safety_risks.tsv.",
         type=str,
-        nargs='?',
-        default=None
+        required=True,
+    )
+    parser.add_argument(
+        "--aopwiki",
+        help="Input JSON containing targets implicated in adverse outcomes as reported by the AOPWiki. Parsed from their source XML data.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--output",
+        help="Output gzipped json file following the target safety liabilities data model.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--log_file",
+        help="Destination of the logs generated by this script. Defaults to None",
+        type=str,
+        nargs="?",
+        default=None,
     )
 
     return parser
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = get_parser().parse_args()
-    
+
     main(
         toxcast=args.toxcast,
         adverse_events=args.adverse_events,
         safety_risk=args.safety_risk,
         aopwiki=args.aopwiki,
-        output=args.output
+        output=args.output,
     )
