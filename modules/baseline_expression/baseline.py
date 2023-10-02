67c0bf2aac1cf35f2ad1875e49df66fbd7a12db7
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-10-02 14:40:40 +0100

    style: lint the main module

--- modules/baseline_expression/baseline.py
+++ modules/baseline_expression/baseline.py
@@ -1,4 +1,4 @@
-"""Ingests GTEx V8 data and computes the baseline expression data."""
+"""Ingests GTEx V8 data and computes the baseline expression metrics."""
 
 import argparse
 import functools
@@ -11,15 +11,18 @@ from . import metrics
 
 
 class BaselineExpression:
+    """Collection of steps and configuration to process the baseline expression data."""
+
     def read_gtex_data(self):
-        """Read and preprocess GTEx source data."""
-        # Read the source data. It's in the GCT format, which is a regular TSV, but the first two lines are metadata which we don't need.
-        self.df = pd.read_csv(self.gtex_source_data, sep="\t", skiprows=2)
+        """Read and preprocess source GTEx data."""
+        # Read the source data. It's in the GCT format, which is a regular TSV, but the first two lines are metadata
+        # which we don't need.
+        self.df = pd.read_csv(self.gtex_source_data_path, sep="\t", skiprows=2)
         # Drop column with gene names, which we don't need at all.
         self.df.drop(columns=["Description"], inplace=True)
         # Drop Ensembl gene version: "ENSG00000223972.2" → "ENSG00000223972".
-        self.df["Name"] = df["Name"].str.split(".").str[0]
-        # Ensembl gene IDs are used as index throughout the entire processing code.
+        self.df["Name"] = self.df["Name"].str.split(".").str[0]
+        # Ensembl gene IDs are used as index throughout the processing code.
         self.df.set_index(keys=["Name"], inplace=True)
 
     def get_name_to_uberon_mapping(self):
@@ -36,9 +39,14 @@ class BaselineExpression:
                 raise AssertionError(f"GTEx tissue {tissue} is not present in the provided mappings file!")
 
     def filter_out_low_expression_genes(self):
-        """Genes with very low expression tend to generate noisy data. One particular case is an expression vector where one tissue has expression of 0.1 TPM and all the other ones are 0.0 exactly. This causes the gene to be called as extremely specifically expressed, even though most of these issues are just false discoveries.
-        A common threshold used for filtering out genes with very low expression is to filter out those where expression in all tissues is less than 1.0 TPM. This is also used by HPA, and supported by internal investigations into quality control.
-        """
+        """Filter out low expression genes.
+
+        Genes with very low expression tend to generate noisy data. One particular case is an expression vector where
+        one tissue has expression of 0.1 TPM and all the other ones are 0.0 exactly. This causes the gene to be called
+        as extremely specifically expressed, even though most of these issues are just false discoveries.
+
+        A range of typical threshold values found in papers is from 0.5 to 5.0 TPM to be considered low expression and
+        dropped."""
         max_expr_across_tissues = self.df.max(axis=1)
         expr_level_mask = max_expr_across_tissues >= self.low_expression_threshold
         self.df = self.df[expr_level_mask]
@@ -58,39 +66,32 @@ class BaselineExpression:
         )
 
     def pack_data_for_output(self):
+        """Pack, output, and compress the data."""
 
         def _remove_adatiss_if_none(d):
-            if d["adatissScores"] != d["adatissScores"]:  # using the fact that NaN != NaN
+            # The below code is using the fact that NaN != NaN.
+            if d["adatissScores"] != d["adatissScores"]:  
                 return {k: v for k, v in d.items() if k != "adatissScores"}
             else:
                 return d
 
-        # Create a list to store JSON objects.
-        json_list = []
-
-        # Iterate over each row index.
-        for idx in self.df.index:
-            # Create a dictionary for the current row.
-            row_dict = {
-                "ensemblGeneId": idx,
-                "expression": [
-                    {
-                        "bodyPartLevel": "tissue",
-                        "bodyPartId": key,
-                        "bodyPartName": self.name_to_uberon_lut[key],
-                        "tpm": value,
-                    }
-                    for key, value in self.df.loc[idx].to_dict().items()
-                ],
-                "expressionSpecificity": _remove_adatiss_if_none(self.specificity.loc[idx].to_dict()),
-            }
-            # Append the dictionary to the list
-            json_list.append(row_dict)
-
         # Output the data.
         with gzip.open(self.output_file_path, "wt", compresslevel=9) as f:
-            for obj in json_list:
-                f.write(json.dumps(obj))
+            for idx in self.df.index:
+                row_dict = {
+                    "ensemblGeneId": idx,
+                    "expression": [
+                        {
+                            "bodyPartLevel": "tissue",
+                            "bodyPartId": key,
+                            "bodyPartName": self.name_to_uberon_lut[key],
+                            "tpm": value,
+                        }
+                        for key, value in self.df.loc[idx].to_dict().items()
+                    ],
+                    "expressionSpecificity": _remove_adatiss_if_none(self.specificity.loc[idx].to_dict()),
+                }
+                f.write(json.dumps(row_dict) + "\n")
                 f.write("\n")
 
     def main(self):
@@ -100,23 +101,21 @@ class BaselineExpression:
         self.calculate_specificity_metrics()
         self.pack_data_for_output()
 
-    def __init__(self, gtex_source_data, tissue_name_to_uberon_mapping, output_file_path, low_expression_threshold):
-        self.gtex_source_data = gtex_source_data
-        self.tissue_name_to_uberon_mapping = tissue_name_to_uberon_mapping
+    def __init__(
+        self, gtex_source_data_path, tissue_name_to_uberon_mapping_path, output_file_path, low_expression_threshold
+    ):
+        self.gtex_source_data_path = gtex_source_data_path
+        self.tissue_name_to_uberon_mapping_path = tissue_name_to_uberon_mapping_path
         self.output_file_path = output_file_path
         self.low_expression_threshold = low_expression_threshold
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--gtex-source-data", required=True, type=str, help="GTEx by-gene median TPM counts, in GCT format")
 parser.add_argument(
-    "--low-expression-threshold",
-    required=True,
-    type=float,
-    help="Genes which are not expressed at least this much in at least one tissue will be filtered out",
+    "--gtex-source-data-path", required=True, type=str, help="GTEx by-gene median TPM counts, in GCT format"
 )
 parser.add_argument(
-    "--tissue-name-to-uberon-mapping",
+    "--tissue-name-to-uberon-mapping-path",
     required=True,
     type=str,
     help="A two column TSV file which maps GTEx tissue names to UBERON IDs.",
@@ -127,11 +126,20 @@ parser.add_argument(
     type=str,
     help="A path to output the output file. GZIP-compressed JSON, one object per line.",
 )
+parser.add_argument(
+    "--low-expression-threshold",
+    required=False,
+    type=float,
+    default=1.0,
+    help="Genes which are not expressed at least this much in at least one tissue will be filtered out.",
+)
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    be = BaselineExpression(
-        args.gtex_source_data, args.low_expression_threshold, args.tissue_name_to_uberon_mapping, args.output_file_path
-    )
-    be.main()
+    BaselineExpression(
+        args.gtex_source_data_path,
+        args.tissue_name_to_uberon_mapping_path,
+        args.output_file_path,
+        args.low_expression_threshold,
+    ).main()
