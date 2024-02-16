8fae5f6fcfa413d059babbafdb3c803486f30d3a
Irene López <irene.lopezs@protonmail.com>
2024-02-16 14:42:45 +0000

    feat(pgx): integrate openai querying into the parser

--- modules/Pharmacogenetics.py
+++ modules/Pharmacogenetics.py
@@ -2,19 +2,21 @@
 """This module adds a more granular description of the phenotype observed in the PharmGKB evidence."""
 
 import argparse
+import json
 import logging
 import sys
-import json
 
 from openai import OpenAI
 import pyspark.sql.functions as f
 from pyspark import SparkFiles
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType
 
 from common.evidence import initialize_sparksession, write_evidence_strings
 from common.ontology import add_efo_mapping
 
-def parse_phenotype(genotype_text: str, openai_client: OpenAI, gpt_model: str = "gpt-3.5-turbo-1106") -> list | None:
+def parse_phenotype_with_gpt(genotype_text: str, openai_client: OpenAI, gpt_model: str = "gpt-3.5-turbo-1106") -> list | None:
+    """Query the OpenAI API to extract the phenotype from the genotype text."""
     prompt = f"""
         Context: We want to analyze PharmGKB clinical annotations. Their data includes a column, "genotypeAnnotationText", which typically informs about efficacy,side effects, or patient response variability given a specific genotype. The data is presented in a lengthy and complex format, making it challenging to quickly grasp the key phenotypic outcomes.
 
@@ -51,32 +53,56 @@ def parse_phenotype(genotype_text: str, openai_client: OpenAI, gpt_model: str = 
         print(f"An error occurred: {e}")
         return None
 
-def main(spark: SparkSession, pharmgkb_evidence_path: str, extracted_phenotypes_path: str, output_file_path: str, cache_dir: str) -> None:
+def parse_phenotypes(texts_to_parse: list[str], openai_client: OpenAI) -> DataFrame:
+    """Parse the phenotypes from the given texts by calling the OpenAI API."""
+    results_dict = {}
+    for text in texts_to_parse:
+        result = parse_phenotype_with_gpt(text, openai_client)
+        if isinstance(result, list):
+            results_dict[text] = result
+        elif isinstance(result, str):
+            results_dict[text] = [result]
+    return spark.createDataFrame(
+        list(results_dict.items()),
+        StructType(
+            [
+                StructField("genotypeAnnotationText", StringType(), True),
+                StructField("phenotypeText", ArrayType(StringType()), True),
+            ]
+        ),
+    )
+
+def update_phenotypes_lut(
+    spark: SparkSession,
+    new_phenotypes_df: DataFrame,
+    extracted_phenotypes_df: DataFrame,
+) -> DataFrame:
+    """Adds the new phenotypes to the extracted phenotypes table."""
+    return (
+        extracted_phenotypes_df.unionByName(new_phenotypes_df)
+    )
+
+
+def generate_evidence(
+    spark: SparkSession,
+    pharmgkb_evidence_df: DataFrame,
+    extracted_phenotypes_df: DataFrame,
+    cache_dir: str
+) -> DataFrame:
     """This module overwrites the `phenotypeText` and adds `diseaseFromSourceMappedId` field in the PharmGKB evidence dataset.
     
     The original `phenotypeText` comes from PharmGKB directly, however it is tipically of little value for the user (more context in https://github.com/opentargets/curation/blob/master/docs/pharmacogenetics.md).
 
     Args:
-        pharmgkb_evidence_path: Input gzipped JSON with the evidence submitted by ChEMBL.
-        extracted_phenotypes_path: Input JSON with the phenotypes extracted from `genotypeAnnotationText`
-        output_file_path: Output gzipped json file containing the pharmgkb evidence with a new `phenotypeText` and `diseaseFromSourceMappedId` fields.
+        pharmgkb_evidence_df: Dataframe with the PGx evidence submitted by EVA.
+        extracted_phenotypes_df: Dataframe containing the phenotypes extracted from `genotypeAnnotationText`
         cache_dir: Directory to store the OnToma cache files in.
     """
-    # Extract
-    logging.info(f"PharmGKB evidence JSON file: {pharmgkb_evidence_path}")
-    logging.info(f"Table of genotype descriptions to extracted phenotypes: {extracted_phenotypes_path}")
-    spark.sparkContext.addFile(extracted_phenotypes_path)
-    pharmgkb_df = spark.read.json(pharmgkb_evidence_path)
-    pgx_phenotypes_df = spark.read.json(SparkFiles.get(extracted_phenotypes_path.split("/")[-1]))
-
-
-    # Transform
     enriched_pharmgkb_df = (
         pharmgkb_df.drop("phenotypeText", "phenotypeFromSourceId").join(pgx_phenotypes_df, on="genotypeAnnotationText", how="left")
         .withColumn("phenotypeText", f.explode("phenotypeText"))
         .persist()
     )
-    assert (enriched_pharmgkb_df.filter(f.col("phenotypeText").isNull()).count() == 0), "There are evidence without a phenotype. Please update the extracted phenotypes table before evidence generation."
     enriched_pharmgkb_df = add_efo_mapping(
         enriched_pharmgkb_df.select("*", f.col("phenotypeText").alias("diseaseFromSource"), f.lit(None).alias("diseaseFromSourceId")),
         spark,
@@ -84,11 +110,8 @@ def main(spark: SparkSession, pharmgkb_evidence_path: str, extracted_phenotypes_
     ).withColumnRenamed("diseaseFromSourceMappedId", "phenotypeFromSourceId").drop("diseaseFromSource")
     logging.info("Disease mappings have been added.")
 
-    # Load
     assert enriched_pharmgkb_df.count() >= pharmgkb_df.count(), "There are fewer evidence after processing."
-    write_evidence_strings(enriched_pharmgkb_df, output_file_path)
-    logging.info(f"{enriched_pharmgkb_df.count()} evidence strings have been saved to {output_file_path}. Exiting.")
-
+    return enriched_pharmgkb_df
 
 def get_parser():
     """Get parser object for script pharmgkb.py."""
@@ -112,7 +135,12 @@ def get_parser():
         required=True
     )
     parser.add_argument(
-        "--output_file_path", help="Output gzipped json file following the target safety liabilities data model.",
+        "--output_evidence_path", help="Output gzipped json file containing the pharmgkb evidence with a new `phenotypeText` and `diseaseFromSourceMappedId` fields.",
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        "--output_phenotypes_path", help="Output json file containing an updated version of the pharmgkb genotype to phenotype automatic annotation. The `curation` repo should be updated with this file.",
         type=str,
         required=True
     )
@@ -135,11 +163,43 @@ if __name__ == "__main__":
     spark = initialize_sparksession()
     args = get_parser().parse_args()
 
-    main(
+    # Read data
+    logging.info(f"PharmGKB evidence JSON file: {args.pharmgkb_evidence_path}")
+    logging.info(f"Table of genotype descriptions to extracted phenotypes: {args.extracted_phenotypes_path}")
+    spark.sparkContext.addFile(args.extracted_phenotypes_path)
+    pgx_phenotypes_df = spark.read.json(SparkFiles.get(args.extracted_phenotypes_path.split("/")[-1]))
+    pharmgkb_df = spark.read.json(args.pharmgkb_evidence_path)
+
+    pgx_df = generate_evidence(
         spark,
-        args.pharmgkb_evidence_path,
-        args.extracted_phenotypes_path,
-        args.openai_api_key,
-        args.output_file_path,
+        pharmgkb_df,
+        pgx_phenotypes_df,
         args.cache_dir
     )
+
+    unparsed_texts = pgx_df.filter(f.col("phenotypeText").isNull()).select("genotypeAnnotationText").distinct()
+    if unparsed_texts.count() == 0:
+        logging.info("All evidence have been correctly parsed.")
+        write_evidence_strings(pgx_df, args.output_evidence_path)
+        logging.info(f"{pgx_df.count()} evidence strings have been saved to {args.output_evidence_path}. Exiting.")
+        
+    else:
+        logging.error("There are evidence without a phenotype. Please update the extracted phenotypes table before evidence generation.")
+        client = OpenAI(api_key=args.openai_api_key)
+        
+        new_phenotypes_df = parse_phenotypes(
+            unparsed_texts.toPandas()["genotypeAnnotationText"].to_list(),
+            client
+        )
+        updated_phenotypes_df = update_phenotypes_lut(spark, new_phenotypes_df, pgx_phenotypes_df)
+        updated_phenotypes_df.coalesce(1).write.json(args.output_phenotypes_path, mode="overwrite")
+        logging.info(f"Updated phenotypes have been saved to {args.output_phenotypes_path}. Exiting.")
+        pgx_df = generate_evidence(
+            spark,
+            pharmgkb_df,
+            updated_phenotypes_df,
+            args.cache_dir
+        )
+
+        
+
